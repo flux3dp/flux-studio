c4a6a2f0b5908877752a407807285424b3855c29
Norman_Chen <nanron0919@gmail.com>
2015-04-28 14:49:00 +0800

    see #5, auto load lang files

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,8 @@ require.config({
         underscore: 'lib/underscore',
         react: 'lib/react/react-with-addons.min',
         JSXTransformer: 'lib/react/JSXTransformer',
-        views: 'app/views/',
-        three: 'lib/three/',
+        views: 'app/views',
+        three: 'lib/three',
         threejs: 'lib/three/three.min'
     },
 
@@ -51,14 +51,16 @@ require([
     'jquery',
     'backbone',
     'app/router',
+    'helpers/local-storage',
     'domReady!',
     'threejs'
-], function($, Backbone, Router) {
+], function($, Backbone, Router, localStorage) {
     'use strict';
 
     var router = new Router();
 
-    Backbone.history.start();
-
-    // load settings
+    // load lang file
+    require(['helpers/i18n'], function(i18n) {
+        Backbone.history.start();
+    });
 });
