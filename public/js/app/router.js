5fe47da97cee6d6ce10e6d294ca64b3c827d9810
Norman_Chen <nanron0919@gmail.com>
2015-04-28 14:52:30 +0800

    see #5, pass props and state to React view

--- public/js/app/router.js
+++ public/js/app/router.js
@@ -1,26 +1,36 @@
 define([
     'react',
     'jquery',
-    'backbone'
+    'backbone',
+    'helpers/local-storage',
+    'helpers/i18n',
+    'helpers/observe'
 ],
-function(React, $, Backbone) {
+function(React, $, Backbone, localStorage, i18n, Observe) {
     'use strict';
     var display = function(view, args, el) {
         args = args || {};
+        args.props = args.props || {};
+        args.state = args.state || {};
         el = el || $('.wrapper')[0];
 
-        view = React.createFactory(view(args));
+        args.state.lang = i18n.get();
+
+        var currentView;
+
+        // watching state and auto update
+        new Observe(args.state, function(changes){
+            setTimeout(function() {
+                currentView.setState(args.state);
+            }, 1000);
+        });
+
+        view = React.createFactory(view(args.state));
         view = view();
 
-        view.props.params = args;
-        view.props.lang = {
-            foo : function() {
-                return 'test';
-            }
-        };
+        view.props = args.props;
 
-        React.render(view, el);
-        console.log(React);
+        currentView = React.render(view, el);
     };
 
     return Backbone.Router.extend({
@@ -30,7 +40,7 @@ function(React, $, Backbone) {
         },
 
         home: function() {
-            require(['jsx!views/Print'], function(View) {
+            require(['jsx!views/Home'], function(View) {
                 display(View);
             });
         }
