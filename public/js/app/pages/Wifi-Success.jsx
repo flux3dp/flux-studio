33890bfb5735ee8ede332587402ea1ef91d7d511
Norman_Chen <nanron0919@gmail.com>
2015-04-29 18:13:44 +0800

    see #11, #12, implement wifi success and failure page

---
+++ public/js/app/pages/Wifi-Success.jsx
@@ -0,0 +1,36 @@
+define([
+    'jquery',
+    'react',
+    'css!cssHome/pages/wifi'
+], function($, React) {
+    'use strict';
+
+    return function(args) {
+        args = args || {};
+
+        var Page = React.createClass({
+                render : function() {
+                    var lang = this.state.lang;
+
+                    return (
+                        <div className="wifi initialization absolute-center">
+                            <h1>{lang.brand_name}</h1>
+                            <div>
+                                <h2>{lang.wifi.success.caption}</h2>
+                                <p>{lang.wifi.success.line1}</p>
+                                <div>
+                                    <a href="#" className="btn">{lang.wifi.success.next}</a>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                },
+                getInitialState: function() {
+                    return args.state;
+                }
+
+            });
+
+        return Page;
+    };
+});
