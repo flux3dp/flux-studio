b9fc1719dcee7e493de0f9c541eda9f6ef5424b9
Norman_Chen <nanron0919@gmail.com>
2015-04-29 10:37:45 +0800

    see #15, add language strings

--- public/js/app/lang/en.js
+++ public/js/app/lang/en.js
@@ -2,8 +2,14 @@ define(function() {
     'use strict';
 
     return {
+        brand_name : 'FLUX',
         app : {
             name : 'Flux Studio - en'
+        },
+        welcome: {
+            header1: 'Hello! Welcome to FLUX. Please choose your preferred language',
+            header2: 'We are getting started from your language',
+            start: 'Start'
         }
     };
 });
--- public/js/app/lang/zh-tw.js
+++ public/js/app/lang/zh-tw.js
@@ -2,8 +2,14 @@ define(function() {
     'use strict';
 
     return {
+        brand_name : 'FLUX',
         app : {
             name : 'Flux Studio - zh-tw'
+        },
+        welcome: {
+            header1: 'Hello! 歡迎使用 FLUX，請選擇你想使用的語言',
+            header2: '我們即將開始為你的 FLUX 進行設定 :)',
+            start: '開始'
         }
     };
 });
