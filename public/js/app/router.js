e3e92fd73fbd9f8c33ba701772d4d84d4692b73b
Norman_Chen <nanron0919@gmail.com>
2015-04-30 16:36:43 +0800

    see #20, set routes with regex and add new route

--- public/js/app/router.js
+++ public/js/app/router.js
@@ -4,22 +4,23 @@ define([
     'backbone',
     'helpers/local-storage',
     'helpers/i18n',
-    'helpers/observe'
+    'helpers/observe',
+    'app/actions/global',
 ],
-function(React, $, Backbone, localStorage, i18n, Observe) {
+function(React, $, Backbone, localStorage, i18n, Observe, globalEvents) {
     'use strict';
     var display = function(view, args, el) {
         args = args || {};
         args.props = args.props || {};
         args.state = args.state || {};
-        el = el || $('.wrapper')[0];
+        el = el || $('.content')[0];
 
         args.state.lang = i18n.get();
 
         var currentView;
 
         // watching state and auto update
-        new Observe(args.state, function(changes){
+        new Observe(args.state, function(changes) {
             currentView.setState(args.state);
         });
 
@@ -29,36 +30,62 @@ function(React, $, Backbone, localStorage, i18n, Observe) {
         view.props = args.props;
 
         currentView = React.render(view, el);
+
+        // WARNING: this function includes GLOBAL LIVE EVENTS.
+        // DO NOT use non-uniqle selector here (e.g. class, tag name, attribute...etc.)
+        globalEvents(args);
     };
 
     return Backbone.Router.extend({
+        routes: {},
 
-        routes: {
-            '': 'home',
-            'initialize/wifi(/:step)' : 'wifi',
+        initialize: function() {
+            var router = this,
+                routes = [
+                    // catch no match route, 404
+                    [/^.*$/, 'e404', this.e404],
+                    // initialize Flux Printer
+                    [
+                        /^initialize\/wifi\/?(ask|select|spot|set-printer|set-password|success|failure)?/,
+                        'initial',
+                        this.initial
+                    ],
+                    // go to studio
+                    [
+                        /^studio\/?(print|laser|scan|usb|settings)\/?(.*)?/,
+                        'studio',
+                        this.studio
+                    ],
+                    // flux home
+                    [/^$/, 'home', this.home],
+                ];
+
+            routes.forEach(function(route) {
+                router.route.apply(router, route);
+            });
         },
 
-        home: function() {
+        home: function(name) {
             require(['jsx!pages/Home', 'app/app-settings'], function(view, settings) {
                 var args = {
-                        props : {
-                            supported_langs : settings.i18n.supported_langs
-                        }
-                    };
+                    props: {
+                        supported_langs: settings.i18n.supported_langs
+                    }
+                };
 
                 display(view, args);
             });
         },
 
-        wifi : function(step) {
+        initial: function(step) {
             var map = {
-                    'ask'          : 'Wifi-Home',
-                    'select'       : 'Wifi-Select',
-                    'spot'         : 'Wifi-Spot',
-                    'set-printer'  : 'Wifi-Set-Printer',
-                    'set-password' : 'Wifi-Set-Password',
-                    'success'      : 'Wifi-Success',
-                    'failure'      : 'Wifi-Failure'
+                    'ask': 'Wifi-Home',
+                    'select': 'Wifi-Select',
+                    'spot': 'Wifi-Spot',
+                    'set-printer': 'Wifi-Set-Printer',
+                    'set-password': 'Wifi-Set-Password',
+                    'success': 'Wifi-Success',
+                    'failure': 'Wifi-Failure'
                 },
                 view_name = 'Wifi-Home';
 
@@ -69,6 +96,31 @@ function(React, $, Backbone, localStorage, i18n, Observe) {
             require(['jsx!pages/' + view_name], function(view) {
                 display(view);
             });
+        },
+
+        appendSideBar: function() {
+            require(['jsx!views/Sidebar'], function(view) {
+                display(view, {}, $('.sidebar')[0]);
+            });
+        },
+
+        studio: function() {
+            this.appendSideBar();
+
+            require(['jsx!pages/Settings', 'app/app-settings'], function(view, settings) {
+                var args = {
+                    props: {
+                        supported_langs: settings.i18n.supported_langs
+                    }
+                };
+
+                display(view, args, $('.content')[0]);
+            });
+        },
+
+        e404: function() {
+            // TODO: handle 404
+            alert('404');
         }
 
     });
