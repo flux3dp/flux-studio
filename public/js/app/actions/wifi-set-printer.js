e4a49778e7ed54865110703e92e87697d7abcd01
Norman_Chen <nanron0919@gmail.com>
2015-05-08 16:54:18 +0800

    see #102, check app was ready or not then do the followings

--- public/js/app/actions/wifi-set-printer.js
+++ public/js/app/actions/wifi-set-printer.js
@@ -1,6 +1,7 @@
 define([
     'jquery',
-    'helpers/local-storage'
+    'helpers/local-storage',
+    'lib/sha256'
 ], function($, localStorage) {
     'use strict';
 
@@ -11,7 +12,11 @@ define([
             // TODO: do validation?
             var $me = $(e.target),
                 $required_fields = $('.required'),
-                is_vaild = true;
+                is_vaild = true,
+                printers = localStorage.get('printers') || [],
+                was_ready = localStorage.get('printer-is-ready') || '';
+
+            was_ready = ('' === was_ready ? false : 'true' === was_ready);
 
             $('.error').removeClass('error');
 
@@ -25,8 +30,22 @@ define([
             });
 
             if (true === is_vaild) {
-                localStorage.set('printer-is-ready', true);
-                location.href = '#studio/print';
+                printers.push(
+                    {
+                        name: $('[name="printer-name"]').val(),
+                        password: CryptoJS.SHA256($('[name="printer-password"]').val()).toString(CryptoJS.enc.Hex)
+                    }
+                );
+                localStorage.set('printers', printers);
+
+                if (true === was_ready) {
+                    $('.popup-window').hide();
+                    location.href = '#studio/settings/printer';
+                }
+                else {
+                    localStorage.set('printer-is-ready', true);
+                    location.href = '#studio/print';
+                }
             }
         });
     };
