d237dec4c5253fe60576eb7b84aa817d48ebcd9c
Norman_Chen <nanron0919@gmail.com>
2015-05-25 14:05:31 +0800

    see #106, binding ui events and react component together and count times of scan

--- public/js/app/pages/Scan.jsx
+++ public/js/app/pages/Scan.jsx
@@ -1,32 +1,122 @@
 define([
     'jquery',
     'react',
-    'app/actions/scan',
+    'jsx!widgets/Popup',
     'jsx!widgets/Select',
     'jsx!widgets/List',
-], function($, React, scanEvents, SelectView, ListView) {
+], function($, React, popup, SelectView, ListView) {
     'use strict';
 
     return function(args) {
         args = args || {};
 
-        var view = React.createClass({
+        var View = React.createClass({
+                // counter
+                scan_times: 0,
+
+                getInitialState: function() {
+                    return args.state;
+                },
+
+                componentDidMount: function() {
+                },
+
+                // ui events
+                _rescan: function(e) {
+                    this.scan_times = 0;
+
+                    this.setState({
+                        scan_times : this.scan_times
+                    });
+                },
+
+                _saveAs: function(e) {
+                    require(['jsx!views/scan/Export'], function(view) {
+                        var popup_window;
+
+                        args.disabledEscapeOnBackground = true;
+
+                        popup_window = popup(view, args);
+                        popup_window.open();
+                    });
+                },
+
+                _startScan: function(e) {
+                    this.scan_times = 1;
+
+                    this.setState({
+                        scan_times : this.scan_times
+                    });
+                },
+
+                _handleScan: function(e) {
+                    var self = this;
+
+                    require(['jsx!views/scan/Progress-Bar'], function(view) {
+                        var popup_window;
+
+                        args.disabledEscapeOnBackground = true;
+
+                        popup_window = popup(view, args);
+                        popup_window.open();
+
+                        // TODO: scan complete
+                        setTimeout(function() {
+                            popup_window.close();
+                            self.scan_times = self.scan_times + 1;
+
+                            self.setState({
+                                scan_times : self.scan_times
+                            });
+                        }, 1000);
+                    });
+                },
+
                 render : function() {
-                    var lang = this.state.lang;
+                    var state = this.state,
+                        cx = React.addons.classSet,
+                        lang = state.lang,
+                        start_scan_text,
+                        header_class,
+                        starting_section,
+                        operating_section;
+
+                    state.scan_times = state.scan_times || 0;
+
+                    start_scan_text = (
+                        1 < state.scan_times
+                        ? lang.scan.start_multiscan
+                        : lang.scan.start_scan
+                    );
+
+                    header_class = cx({
+                        'scan-herader' : true,
+                        'invisible'    : 2 > state.scan_times
+                    });
+
+                    starting_section = cx({
+                        'starting-section' : true,
+                        'hide' : 0 < state.scan_times
+                    });
+
+                    operating_section = cx({
+                        'operating-section' : true,
+                        'hide' : 0 === state.scan_times
+                    });
 
                     return (
                         <div className="studio-container scan-studio">
-                            <header className="scan-herader invisible">
-                                <button className="btn fa fa-undo" id="btn-rescan">{lang.scan.rescan}</button>
-                                <button className="btn fa fa-paper-plane" id="btn-export">{lang.scan.export}</button>
+                            <header ref="header" className={header_class}>
+                                <button className="btn fa fa-undo" onClick={this._rescan}>{lang.scan.rescan}</button>
+                                <button className="btn fa fa-paper-plane" onClick={this._saveAs}>{lang.scan.export}</button>
                                 <button className="btn fa fa-floppy-o">{lang.scan.share}</button>
                                 <button className="btn fa fa-eye">{lang.scan.print_with_flux}</button>
                             </header>
                             <div className="section-container">
-                                <section className="starting-section">
-                                    <img className="launch-img absolute-center" src="http://placehold.it/280x193"/>
+                                <section className={starting_section}>
+                                    <img className="launch-img absolute-center" src="http://placehold.it/280x193" onClick={this._startScan}/>
                                 </section>
-                                <section className="operating-section hide">
+                                <section className={operating_section}>
                                     <div id="operating-panel" className="operating-panel">
                                         <div className="panel print-params">
                                             <div>
@@ -68,8 +158,8 @@ define([
                                                 </div>
                                             </div>
                                             <div>
-                                                <button id="btn-scan" className="btn span12 fa fa-bullseye">
-                                                    {lang.scan.start_scan}
+                                                <button id="btn-scan" onClick={this._handleScan} className="btn span12 fa fa-bullseye">
+                                                    {start_scan_text}
                                                 </button>
                                             </div>
                                         </div>
@@ -79,16 +169,10 @@ define([
                             </div>
                         </div>
                     )
-                },
-                getInitialState: function() {
-                    return args.state;
-                },
-                componentDidMount: function() {
-                    scanEvents(args);
                 }
 
             });
 
-        return view;
+        return View;
     };
 });
