22134d344649d0955e154ce97cc0a7d9078ad180
Norman_Chen <nanron0919@gmail.com>
2015-05-25 16:35:02 +0800

    see #154, refactoring

--- public/js/helpers/websocket.js
+++ public/js/helpers/websocket.js
@@ -1,26 +1,48 @@
-define(['jquery'], function($) {
+define(function($) {
     'use strict';
 
-    var url = 'ws://localhost:8080/ws/';
+    var defaultOptions = {
+            hostname: location.hostname,
+            method: '',
+            port: '8000',
+            onMessage: function(result) {},
+            onClose: function(result) {}
+        },
+        origanizeOptions = function(opts) {
+            for (var name in defaultOptions) {
+                if (false === opts.hasOwnProperty(name)) {
+                    opts[name] = defaultOptions[name];
+                }
+            }
 
-    return function(method, onmessage) {
-        var ws = new WebSocket(url + method),
-            _onmessage = onmessage || function() {};
-
-        ws.onmessage = function(result) {
-            _onmessage(result);
+            return opts;
         };
 
-        ws.onclose = function(v) {
-            // TODO: do something
-            console.log('CONNECTION CLOSED');
+    // options:
+    //      hostname  - host name (Default: localhost)
+    //      port      - what protocol uses (Default: 8000)
+    //      method    - method be called
+    //      onMessage - fired on receive message
+    //      onClose   - fired on connection closed
+    return function(options) {
+        options = origanizeOptions(options);
+
+        var url = 'ws://' + options.hostname + ':' + options.port + '/ws/' + options.method,
+            ws = new WebSocket(url);
+
+        ws.onmessage = function(result) {
+            // TODO: logging
+            options.onMessage(result);
+        };
+
+        ws.onclose = function(result) {
+            // TODO: logging
+            options.onClose(result);
             ws = null;  // release
         };
 
         return {
-            send : function(data, onmessage) {
-                _onmessage = onmessage || _onmessage || function() {};
-
+            send: function(data) {
                 var wait = 0,
                     retry_times = 1000,
                     timer = setInterval(function() {
@@ -36,7 +58,7 @@ define(['jquery'], function($) {
                             }
                         }
                         catch (ex) {
-                            // TODO: do something
+                            // TODO: logging
                         }
 
                         if (retry_times <= 0) {
@@ -46,11 +68,28 @@ define(['jquery'], function($) {
                         wait = 100;
 
                     }, wait);
+
+                return this;
             },
-            close : function() {
+
+            close: function() {
                 if (null !== ws) {
                     ws.close();
                 }
+            },
+
+            // events
+            onMessage: function(callback) {
+                console.log('callback');
+                options.onMessage = callback;
+
+                return this;
+            },
+
+            onClose: function(callback) {
+                options.onclose = callback;
+
+                return this;
             }
         };
     };
