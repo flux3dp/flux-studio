aaa149aa9ec8ad1ca1fc5da1e4a8da423ec23811
Norman_Chen <nanron0919@gmail.com>
2015-06-22 18:50:11 +0800

    see #227, add helpers, grayscale and convertToTypedArray

---
+++ public/js/helpers/grayscale.js
@@ -0,0 +1,35 @@
+/**
+ * grayscale
+ */
+define(function() {
+    'use strict';
+
+    return function(data, opts) {
+        opts = opts || {};
+        opts.is_rgba = ('boolean' === typeof opts.is_rgba ? opts.is_rgba : false);
+        opts.threshold = ('number' === typeof opts.threshold ? opts.threshold : 128);
+
+        var binary = [],
+            WHITE = 255;
+
+        for (var i = 0; i < data.length; i += 4) {
+            // refers to http://en.wikipedia.org/wiki/Grayscale
+            var grayscale = parseInt(data[i] * 0.299 + data[i + 1] * 0.587 + data[i + 2] * 0.114, 10);
+
+            grayscale = (opts.threshold < grayscale ? grayscale : grayscale * 1.5);
+
+            if (false === opts.is_rgba) {
+                binary.push(grayscale);
+            }
+            else {
+                for (var j = 0; j < 3; j++) {
+                    binary.push(grayscale);
+                }
+
+                binary.push(data[i + 3]);
+            }
+        }
+
+        return binary;
+    };
+});
