c18742c1f818d103a4b0c1823450701fd00681b1
Norman <nanron0919@gmail.com>
2015-07-31 17:16:25 +0800

    see #349, dont show progress when all process done

--- public/js/app/views/scan/Progress-Bar.jsx
+++ public/js/app/views/scan/Progress-Bar.jsx
@@ -7,11 +7,39 @@ define([
         args = args || {};
 
         var Widget = React.createClass({
+                _renderProgress: function() {
+                    var lang = this.state.lang;
+
+                    return (
+                        <p>
+                            <span>{lang.scan.complete}: </span>
+                            <span>{this.state.progressPercentage}%, </span>
+                            <span>{lang.scan.remaining_time}: </span>
+                            <span>{this.state.progressRemainingTime}</span>
+                        </p>
+                    );
+                },
+                _renderFinish: function() {
+                    var lang = this.state.lang;
+
+                    return (
+                        <p>
+                            <span>{lang.scan.complete}</span>
+                        </p>
+                    );
+                },
+
                 render : function() {
                     var lang = this.state.lang,
+                        is_finished = this.state.is_finished,
                         style = {
                             width: this.state.progressPercentage + '%'
-                        };
+                        },
+                        content = (
+                            true === is_finished ?
+                            this._renderFinish() :
+                            this._renderProgress()
+                        );
 
                     return (
                         <div className="scan-progress absolute-center">
@@ -19,12 +47,8 @@ define([
                             <div className="progress">
                                 <div className="progress-bar progress-bar-striped active" style={style}/>
                             </div>
-                            <p>
-                                <span>{lang.scan.complete}: </span>
-                                <span>{this.state.progressPercentage}%, </span>
-                                <span>{lang.scan.remaining_time}: </span>
-                                <span>{this.state.progressRemainingTime}</span>
-                            </p>
+
+                            {content}
                         </div>
                     );
                 },
