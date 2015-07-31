ace1a4bec97ee4993bcd3899bb854062102de30b
Norman <nanron0919@gmail.com>
2015-07-31 17:57:07 +0800

    see #348, redirect to studio/print if app was initialized

--- public/js/app/actions/global.js
+++ public/js/app/actions/global.js
@@ -18,12 +18,17 @@ define([
 
         (function() {
             var $body = $('body'),
-                is_ready = localStorage.get('printer-is-ready') || '';
+                is_ready = localStorage.get('printer-is-ready') || '',
+                hash = location.hash;
 
             is_ready = ('' === is_ready ? false : 'true' === is_ready);
 
             if (true === is_ready) {
                 $body.addClass('is-ready');
+
+                if ('' === hash || hash.startsWith('#initialize')) {
+                    location.hash = '#studio/print';
+                }
             }
             else {
                 $body.removeClass('is-ready');
