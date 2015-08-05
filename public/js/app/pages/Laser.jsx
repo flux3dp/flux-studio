74203a145c3212f229bed2dea0b232475efe9511
Norman <nanron0919@gmail.com>
2015-08-05 15:53:04 +0800

    see #345, add advanced panel and gets parameters from it

--- public/js/app/pages/Laser.jsx
+++ public/js/app/pages/Laser.jsx
@@ -4,20 +4,37 @@ define([
     'app/actions/laser',
     'jsx!widgets/Select',
     'jsx!views/laser/Setup-Panel',
-    'jsx!views/laser/Image-Panel'
-], function($, React, laserEvents, SelectView, SetupPanel, ImagePanel) {
+    'jsx!views/laser/Image-Panel',
+    'jsx!widgets/Modal',
+    'jsx!views/Print-Selector'
+], function(
+    $,
+    React,
+    laserEvents,
+    SelectView,
+    SetupPanel,
+    ImagePanel,
+    Modal,
+    PrinterSelector
+) {
     'use strict';
 
     return function(args) {
         args = args || {};
 
         var view = React.createClass({
+                _setupSettings: undefined,
+
+                _saveSettings: function(settings) {
+                    this._setupSettings = settings;
+                },
+
                 _renderHeader: function() {
                     var lang = args.state.lang,
                         cx = React.addons.classSet,
                         export_file_class = cx({
                             'btn btn-default fa fa-floppy-o': true,
-                            'hide': false === this.state.has_image
+                            'hide': false === this.state.hasImage
                         });
 
                     return (
@@ -36,27 +53,68 @@ define([
                     var lang = args.state.lang,
                         cx = React.addons.classSet,
                         image_panel_class = cx({
-                            'hide': false === this.state.has_image,
+                            'hide': false === this.state.hasImage,
                             'panel object-position': true
                         });
 
                     return (
                         <section id="operation-table">
                             <div className="laser-object border-circle"/>
-                            <SetupPanel lang={lang} mode={this.state.mode} className='operating-panel' hasImage={this.state.has_image}/>
+                            <SetupPanel
+                                lang={lang}
+                                mode={this.state.mode}
+                                className='operating-panel'
+                                hasImage={this.state.hasImage}
+                                getSettings={this._saveSettings}
+                            />
                             <ImagePanel lang={lang} className={image_panel_class}/>
                         </section>
                     );
                 },
-                render : function() {
+
+                _renderPrinterSelectorWindow: function(lang) {
+                    var self = this,
+                        onGettingPrinter = function(auth_printer) {
+                            self.setState({
+                                selectedPrinter: auth_printer,
+                                openPrinterSelectorWindow: false
+                            });
+                            self.props.doLaser(self._setupSettings);
+                        },
+                        content = (
+                            <PrinterSelector lang={lang} onGettingPrinter={onGettingPrinter}/>
+                        ),
+                        onClose = function(e) {
+                            self.setState({
+                                openPrinterSelectorWindow: false
+                            });
+                        };
+
+                    return (
+                        <Modal content={content} onClose={onClose}/>
+                    );
+                },
+
+                render: function() {
                     var lang = args.state.lang,
                         header = this._renderHeader(),
-                        stageSection = this._renderStageSection();
+                        stageSection = this._renderStageSection(),
+                        printerSelector = (
+                            true === this.state.openPrinterSelectorWindow ?
+                            this._renderPrinterSelectorWindow(lang) :
+                            ''
+                        );
+
+                    this._setupSettings = lang.laser.advanced.form.object_options.options.filter(
+                        function(obj) {
+                            return true === obj.selected;
+                        }
+                    )[0].data;
 
                     return (
                         <div className="studio-container laser-studio">
-
                             {header}
+                            {printerSelector}
 
                             <div className="stage">
                                 {stageSection}
@@ -64,13 +122,17 @@ define([
                         </div>
                     );
                 },
+
                 getInitialState: function() {
                     return {
                         step: '',
                         mode: 'engrave',
-                        has_image: false
+                        hasImage: false,
+                        selectedPrinter: 0,
+                        openPrinterSelectorWindow: false
                     };
                 },
+
                 componentDidMount: function() {
                     laserEvents(args, this);
                 }
