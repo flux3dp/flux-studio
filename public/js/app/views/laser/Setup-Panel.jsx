e807b976e04b3e8fe8b3af831030d4f90c9725d2
Norman <nanron0919@gmail.com>
2015-08-05 15:57:15 +0800

    see #345, add advanced panel and use modal instead

--- public/js/app/views/laser/Setup-Panel.jsx
+++ public/js/app/views/laser/Setup-Panel.jsx
@@ -1,10 +1,69 @@
 define([
+    'jquery',
     'react',
-    'jsx!widgets/Select'
-], function(React, SelectView) {
+    'jsx!widgets/Select',
+    'jsx!widgets/Modal',
+    'jsx!views/laser/Advanced-Panel'
+], function($, React, SelectView, Modal, AdvancedPanel) {
     'use strict';
 
     return React.createClass({
+        _getSelectedMaterial: function(value) {
+            var props = this.props,
+                lang = props.lang,
+                selected_material = lang.laser.advanced.form.object_options.options.filter(
+                    function(obj) {
+                        return value === obj.value;
+                    }
+                );
+
+            return (0 < selected_material.length ? selected_material[0] : undefined);
+        },
+
+        _toggleAdvancedPanel: function(open) {
+            var self = this;
+
+            return function(material_name) {
+                material_name = material_name || '';
+
+                self.setState({
+                    openAdvancedPanel: open,
+                    defaultMaterial: self._getSelectedMaterial(material_name)
+                });
+            };
+        },
+
+        _openAdvancedPanel: function(e) {
+            var $material = $(this.refs.material.getDOMNode()),
+                selected_value = $material.find('option:selected').val();
+
+            this._toggleAdvancedPanel(true)(selected_value);
+        },
+
+        _onDone: function(settings) {
+            this.props.getSettings(settings);
+
+            this._toggleAdvancedPanel(false)(settings.material);
+        },
+
+        _renderAdvancedPanel: function(lang, default_material) {
+            var content = (
+                    <AdvancedPanel
+                        lang={lang}
+                        materials={this.state.materials}
+                        defaultMaterial={default_material}
+                        onCancel={this._toggleAdvancedPanel(false)}
+                        onDone={this._onDone}
+                    />
+                );
+
+            return (
+                true === this.state.openAdvancedPanel ?
+                <Modal content={content} onClose={this._toggleAdvancedPanel(false)}/> :
+                ''
+            );
+        },
+
         render: function() {
             var props = this.props,
                 lang = props.lang,
@@ -13,7 +72,16 @@ define([
                 button_class = cx({
                     'btn btn-action btn-full-width btn-start': true,
                     'btn-disabled': false === props.hasImage
-                });
+                }),
+                default_material = (
+                    this.state.defaultMaterial ||
+                    lang.laser.advanced.form.object_options.options.filter(
+                        function(obj) {
+                            return true === obj.selected;
+                        }
+                    )[0]
+                ),
+                advancedPanel = this._renderAdvancedPanel(lang, default_material);
 
             return (
                 <div className="setup-panel operating-panel">
@@ -22,9 +90,14 @@ define([
                         <div className="setup">
                             <div className="icon print-speed"></div>
                             <div className="controls">
-                                <div className="label">{lang.laser.print_params.material.text}</div>
+                                <div className="label">{lang.laser.advanced.form.object_options.text}</div>
                                 <div className="control">
-                                    <SelectView className="span12" name="material" options={lang.laser.print_params.material.options}/>
+                                    <SelectView
+                                        defaultValue={default_material.value}
+                                        ref="material"
+                                        className="span12"
+                                        options={this.state.materials}
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -39,15 +112,36 @@ define([
                             </div>
                         </div>
                         <div className="setup last-setup">
-                            <button className="btn btn-default btn-full-width">{lang.laser.advenced}</button>
+                            <button className="btn btn-default btn-full-width" onClick={this._openAdvancedPanel}>{lang.laser.advenced}</button>
                         </div>
                     </div>
                     <button id="btn-start" className={button_class}>
                         <img src="/img/icon-laser-s.png"/>
                         {mode}
                     </button>
+                    {advancedPanel}
                 </div>
             );
+        },
+
+        getDefaultProps: function() {
+            var self = this;
+
+            return {
+                settingMaterial: React.PropTypes.object,
+                getSettings: React.PropTypes.func
+            };
+        },
+
+        getInitialState: function() {
+            var props = this.props,
+                lang = props.lang;
+
+            return {
+                openAdvancedPanel: false,
+                defaultMaterial: undefined,
+                materials: lang.laser.advanced.form.object_options.options
+            };
         }
 
     });
