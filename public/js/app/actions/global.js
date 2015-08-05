d071cbc2dc03a2d952b59ee8090ad668cec64068
Norman <nanron0919@gmail.com>
2015-08-05 19:32:58 +0800

    see #348, check printer is ready

--- public/js/app/actions/global.js
+++ public/js/app/actions/global.js
@@ -13,26 +13,18 @@ define([
     });
 
     return function(callback) {
-        var global_config = config(),
-            $body = $('body'),
+        var $body = $('body'),
             hash = location.hash;
 
-        global_config.read('printer-is-ready', {
+        config().read('printer-is-ready', {
             onFinished: function(data) {
 
                 var is_ready = data;
 
                 is_ready = ('' === is_ready ? false : 'true' === is_ready);
 
-                if (true === is_ready) {
-                    $body.addClass('is-ready');
-
-                    if ('' === hash || hash.startsWith('#initialize')) {
-                        location.hash = '#studio/print';
-                    }
-                }
-                else {
-                    $body.removeClass('is-ready');
+                if (true === is_ready && ('' === hash || hash.startsWith('#initialize'))) {
+                    location.hash = '#studio/print';
                 }
 
                 callback();
