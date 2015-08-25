b4e5ba6bc89e5661068ca2c46850897e47bb4ec6
Norman <nanron0919@gmail.com>
2015-08-25 15:28:19 +0800

    see #439, trigger event on body

--- public/js/helpers/shortcuts.js
+++ public/js/helpers/shortcuts.js
@@ -37,30 +37,31 @@ define([
         keydown_event = function(e) {
             var matches = [];
 
-            if (true === e.metaKey) {
-                keyup_event();
-                keyCodeStatus.push(special_key_map.CMD);
+            if ('BODY' === e.target.tagName) {
+                if (true === e.metaKey) {
+                    keyup_event();
+                    keyCodeStatus.push(special_key_map.CMD);
 
-                if (false === isMetaKey(e.keyCode)) {
+                    if (false === isMetaKey(e.keyCode)) {
+                        keyCodeStatus.push(e.keyCode);
+                    }
+                }
+                else {
                     keyCodeStatus.push(e.keyCode);
                 }
+
+                keyCodeStatus = keyCodeStatus.unique().sort();
+
+                matches = matchedEvents(keyCodeStatus);
+
+                if (0 < matches.length) {
+                    keyCodeStatus = [];
+                }
+
+                matches.forEach(function(event, index) {
+                    event.callback.apply(null, [e]);
+                });
             }
-            else {
-                keyCodeStatus.push(e.keyCode);
-            }
-
-            keyCodeStatus = keyCodeStatus.unique().sort();
-
-            matches = matchedEvents(keyCodeStatus);
-
-            if (0 < matches.length) {
-                e.preventDefault();
-                keyCodeStatus = [];
-            }
-
-            matches.forEach(function(event, index) {
-                event.callback.apply(null, [e]);
-            });
         },
         initialize = function() {
             if (false === has_bind) {
