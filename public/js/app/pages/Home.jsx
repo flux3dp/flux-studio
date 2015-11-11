eee8ccc8e7d5557ddf5b39f6040a2b2026b89752
Norman <nanron0919@gmail.com>
2015-11-11 18:22:02 +0800

    see #671, modify as pure page without any actions

--- public/js/app/pages/Home.jsx
+++ public/js/app/pages/Home.jsx
@@ -1,59 +1,16 @@
 define([
     'react',
     'helpers/i18n',
-    'helpers/local-storage',
     'jsx!widgets/Select',
-    'jsx!widgets/Modal',
-    'helpers/api/config',
-    'helpers/api/usb-config'
-], function(React, i18n, localStorage, SelectView, Modal, config, usbConfig) {
+    'jsx!widgets/Modal'
+], function(React, i18n, SelectView, Modal) {
     'use strict';
 
     return function(args) {
         args = args || {};
 
         return React.createClass({
-            // UI events
-            _onSkipSettingUp: function(e) {
-                var goNext = function() {
-                    location.hash = '#studio/print/';
-                };
-
-                config().write('printer-is-ready', true, {
-                    onFinished: function() {
-                        goNext();
-                    }
-                });
-            },
-
-            _onStartingSetUp: function(e) {
-                var self = this,
-                    usb = usbConfig(),
-                    toggleBlocker = function(open) {
-                        self.setState({
-                            openBlocker: open
-                        });
-                    },
-                    goNext = function(printer) {
-                        // temporary store for setup
-                        localStorage.set('setting-printer', printer);
-                        location.hash = '#initialize/wifi/ask';
-                    };
-
-                toggleBlocker(true);
-
-                usb.list({
-                    onSuccess: function(response) {
-                        toggleBlocker(false);
-                        goNext(response);
-                    },
-                    onError: function(response) {
-                        toggleBlocker(false);
-                        // TODO: when this function is fired that means no machine available.
-                    }
-                });
-            },
-
+            // Private methods
             _getLanguageOptions: function() {
                 var options = [];
 
@@ -68,22 +25,9 @@ define([
                 return options;
             },
 
-            // renders
-            _renderBlocker: function() {
-                var content = (
-                    <div className="spinner-flip"/>
-                );
-
-                return (
-                    true === this.state.openBlocker ?
-                    <Modal content={content} disabledEscapeOnBackground={false}/> :
-                    ''
-                );
-            },
-
-            render : function() {
+            // Lifecycle
+            render: function() {
                 var lang = this.state.lang,
-                    blocker = this._renderBlocker(),
                     options = this._getLanguageOptions(),
                     content = (
                         <div className="welcome initialization text-center">
@@ -96,13 +40,9 @@ define([
                                     <SelectView id="select-lang" options={options}/>
                                 </div>
                                 <div>
-                                    <button className="btn btn-action btn-large" onClick={this._onStartingSetUp}>{lang.welcome.start}</button>
-                                </div>
-                                <div>
-                                    <button className="btn btn-link" onClick={this._onSkipSettingUp}>{lang.welcome.skip}</button>
+                                    <a href="#initialize/wifi/connect-machine" className="btn btn-action btn-large">{lang.welcome.start}</a>
                                 </div>
                             </div>
-                            {blocker}
                         </div>
                     );
 
@@ -113,8 +53,7 @@ define([
 
             getInitialState: function() {
                 return {
-                    lang: args.state.lang,
-                    openBlocker: false
+                    lang: args.state.lang
                 };
             }
         });
