b94e855fe71074412bb3e021d90a9649c2cfc011
Norman <nanron0919@gmail.com>
2015-12-04 13:03:14 +0800

    see #816, implement input dialog with flux

---
+++ public/js/app/constants/input-lightbox-constants.js
@@ -0,0 +1,11 @@
+define(function() {
+    'use strict';
+
+    return {
+        TYPE_TEXT   : 'TEXT_INPUT',
+        TYPE_NUMBER : 'NUMBER_INPUT',
+
+        // event
+        OPEN_EVENT  : 'OPEN_INPUT_LIGHTBOX'
+    };
+});
