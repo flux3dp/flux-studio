7ac39384079dfc40584b09c74ab16cf98d674776
Norman <nanron0919@gmail.com>
2015-12-06 17:17:57 +0800

    see #828, check which port from 8000 is available

--- public/nw-inject.js
+++ public/nw-inject.js
@@ -1,34 +1,86 @@
-var gui = require('nw.gui'),
-    currentWindow = gui.Window.get(),
-    fs = require('fs'),
+var fs = require('fs'),
     os = require('os'),
     spawn = require('child_process').spawn,
     exec = require('child_process').exec,
     cwd = process.cwd(),
-    ghost;
+    net = require('net'),
+    currentPort = 8000,
+    maxPort = 65535,
+    gui = require('nw.gui'),
+    currentWindow = gui.Window.get(),
+    ghost,
+    appWindow,
+    executeGhost = function(port) {
+        var slic3rPathIndex = 2,
+            args = [
+                '-s',
+                '--slic3r',
+                '',
+                '--port',
+                port,
+                '--assets',
+                cwd + 'lib/ghost/assets'
+            ],
+            ghostCmd = '';
 
-console.log(process);
-if ('Windows_NT' === os.type()) {
-    fs.chmodSync(cwd + '/lib/ghost/ghost/ghost.exe', 0777);
-    ghost = spawn(cwd + '/lib/ghost/ghost/ghost.exe', ['-s']);
-    // TODO: has to assign env root for slic3r
-}
-else {
-    fs.chmodSync(cwd + '/lib/ghost/ghost/ghost', 0777);
-    ghost = spawn(cwd + '/lib/ghost/ghost/ghost', ['-s', '--slic3r', cwd + '/lib/Slic3r.app/Contents/MacOS/slic3r']);
-}
+        if ('Windows_NT' === os.type()) {
+            // TODO: has to assign env root for slic3r
+            args[slic3rPathIndex] = cwd + '/lib/Slic3r.app/Contents/MacOS/slic3r';
+            ghostCmd = cwd + '/lib/ghost/ghost/ghost.exe';
+        }
+        else {
+            args[slic3rPathIndex] = cwd + '/lib/Slic3r.app/Contents/MacOS/slic3r';
+            ghostCmd = cwd + '/lib/ghost/ghost/ghost';
+        }
 
-ghost.stdout.on('data', function(data) {
-    console.log('stdout: ' + data);
-});
+        fs.chmodSync(ghostCmd, 0777);
+        ghost = spawn(ghostCmd, args);
 
-ghost.stderr.on('data', function(data) {
-    console.log('stderr: ' + data);
-});
+        ghost.stdout.on('data', function(data) {
+            console.log('stdout: ' + data);
+        });
 
-ghost.on('exit', function (code) {
-    console.log('child process exited with code ' + code);
-});
+        ghost.stderr.on('data', function(data) {
+            console.log('stderr: ' + data);
+        });
+
+        ghost.on('exit', function (code) {
+            console.log('child process exited with code ' + code);
+        });
+
+        process.env.ghostPort = port;
+    },
+    probe = function(port, callback) {
+        var socket = new net.Socket(),
+            portAvailable = true;
+
+        socket.connect(port, '127.0.0.1', function() {
+            portAvailable = false;
+
+            socket.end(function() {
+                callback(portAvailable);
+            });
+        });
+
+        socket.on('error', function() {
+            callback(portAvailable);
+        });
+    },
+    findPort = function(result) {
+        if (true === result) {
+            executeGhost(currentPort);
+        }
+        else if (currentPort < maxPort) {
+            currentPort++;
+            probe(currentPort, findPort);
+        }
+        else {
+            // stop trying and response error
+        }
+    };
+
+// find port
+probe(currentPort, findPort);
 
 currentWindow.on('close', function() {
     // Pretend to be closed already
