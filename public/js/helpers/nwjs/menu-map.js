a6ff7717c1ed66cfec50684527438b97835ba801
Norman <nanron0919@gmail.com>
2015-12-08 18:37:54 +0800

    reorganize menu items

--- public/js/helpers/nwjs/menu-map.js
+++ public/js/helpers/nwjs/menu-map.js
@@ -7,8 +7,21 @@ define([
     'helpers/check-firmware',
     'helpers/api/config',
     'helpers/api/discover',
-    'app/actions/alert-actions'
-], function(gui, i18n, checkFirmware, config, discover, AlertActions) {
+    'app/actions/alert-actions',
+    'app/actions/global-actions',
+    'helpers/device-master',
+    'app/constants/device-constants'
+], function(
+    gui,
+    i18n,
+    checkFirmware,
+    config,
+    discover,
+    AlertActions,
+    GlobalActions,
+    DeviceMaster,
+    DeviceConstants
+) {
     'use strict';
 
     var emptyFunction = function(object) {
@@ -20,6 +33,15 @@ define([
         },
         lang = i18n.get().topmenu,
         menuMap = [],
+        newDevice = {
+            label: lang.device.new,
+            enabled: true,
+            onClick: function() {
+                location.hash = '#initialize/wifi/connect-machine';
+            },
+            key: 'n',
+            modifiers: 'cmd'
+        },
         items = {
             import: {
                 label: lang.file.import,
@@ -106,19 +128,13 @@ define([
                 enabled: false,
                 onClick: emptyFunction
             },
-            newDevice: {
-                label: lang.device.new,
+            newDevice: newDevice,
+            device: {
+                label: lang.device.label,
                 enabled: true,
-                onClick: function() {
-                    location.hash = '#initialize/wifi/connect-machine';
-                },
-                key: 'n',
-                modifiers: 'cmd'
+                subItems: [newDevice, separator]
             }
-        },
-        deviceGroup = [
-            items.newDevice
-        ];
+        };
 
     function bindMap() {
         menuMap = [];
@@ -193,10 +209,7 @@ define([
                     items.viewPreview
                 ]
             },
-            {
-                label: lang.device.label,
-                subItems: deviceGroup
-            },
+            items.device,
             {
                 label: lang.window.label,
                 subItems: [
@@ -220,10 +233,19 @@ define([
             });
 
             if ('undefined' !== typeof requireNode) {
+                var deviceGroup = [
+                        newDevice,
+                        separator
+                    ],
+                    deviceRefreshTimer;
+
                 discover(
                     'menu-map',
                     function(printers) {
-                        deviceGroup = [items.newDevice, separator];
+                        deviceGroup = [
+                            newDevice,
+                            separator
+                        ];
 
                         printers.forEach(function(printer) {
                             deviceGroup.push({
@@ -233,7 +255,14 @@ define([
                                     label: lang.device.device_monitor,
                                     enabled: true,
                                     onClick: function() {
-                                        // TODO: go to monitor
+                                        DeviceMaster.selectDevice(printer).then(function(status) {
+                                            if(status === DeviceConstants.CONNECTED) {
+                                                GlobalActions.showMonitor(printer);
+                                            }
+                                            else if (status === DeviceConstants.TIMEOUT) {
+                                                AlertActions.showPopupError('menu-item', lang.message.connectionTimeout);
+                                            }
+                                        });
                                     }
                                 },
                                 {
@@ -256,6 +285,14 @@ define([
                                 }]
                             });
                         });
+
+                        if ('undefined' === typeof deviceRefreshTimer) {
+                            clearTimeout(deviceRefreshTimer);
+                            setTimeout(function() {
+                                items.device.subItems = deviceGroup;
+                                clearTimeout(deviceRefreshTimer);
+                            }, 10000);
+                        }
                     }
                 );
             }
