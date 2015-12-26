f418f0ae51ead77cd69fd52372346fdf0c193b4a
Norman <nanron0919@gmail.com>
2015-12-26 20:03:04 +0800

    see #944, keycode map

---
+++ public/js/app/constants/keycode-constants.js
@@ -0,0 +1,26 @@
+/**
+ * key code map
+ */
+define(function() {
+    'use strict';
+
+    return {
+        KEY_L_CMD    : 91,
+        KEY_R_CMD    : 93,
+        KEY_RETURN   : 13,  // enter
+        KEY_PLUS     : 187,
+        KEY_MINUS    : 189,
+        KEY_MULTIPLY : 56,
+        KEY_DIVIDE   : 191,
+        KEY_SHIFT    : 16,
+        KEY_CTRL     : 17,
+        KEY_ALT      : 18,
+        KEY_DEL      : 8,
+        KEY_TAB      : 9,
+        KEY_ESC      : 27,
+        KEY_LEFT     : 37,
+        KEY_UP       : 38,
+        KEY_RIGHT    : 39,
+        KEY_DOWN     : 40
+    };
+});
