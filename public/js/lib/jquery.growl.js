84d71d04a4597e348483fe371312eb4679fba45d
simonxeko <simonxeko@gmail.com>
2016-01-01 03:45:14 +0800

    Modify notification duration

--- public/js/lib/jquery.growl.js
+++ public/js/lib/jquery.growl.js
@@ -42,7 +42,7 @@ Copyright 2015 Kevin Sylvestre
   Growl = (function() {
     Growl.settings = {
       namespace: 'growl',
-      duration: 4000,
+      duration: 10000,
       close: "&#215;",
       location: "default",
       style: "default",
