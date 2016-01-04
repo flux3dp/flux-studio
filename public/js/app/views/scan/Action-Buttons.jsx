ec74a40fe9af9b88cadb65bb12e3a80333f0493d
Norman <nanron0919@gmail.com>
2016-01-04 13:57:28 +0800

    see #804, remove save pcd button in action buttons

--- public/js/app/views/scan/Action-Buttons.jsx
+++ public/js/app/views/scan/Action-Buttons.jsx
@@ -74,17 +74,6 @@ define([
                     },
                     onClick: self.props.onConvertClick
                 });
-
-                if (true === window.FLUX.debug) {
-                    buttons.push({
-                        label: 'Save PCD',
-                        className: 'btn-action btn-hexagon btn-save-pcd',
-                        dataAttrs: {
-                            'ga-event': 'save-as-pcd'
-                        },
-                        onClick: self.props.onSaveClick
-                    });
-                }
                 break;
             case 'CONVERTED':
                 buttons.push({
