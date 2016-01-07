67305386f4f71d281b348402378e5a2e2a061803
Norman <nanron0919@gmail.com>
2016-01-07 18:19:41 +0800

    see #1071, add file iput to select single file

--- public/js/app/constants/input-lightbox-constants.js
+++ public/js/app/constants/input-lightbox-constants.js
@@ -5,6 +5,7 @@ define(function() {
         TYPE_TEXT     : 'TEXT_INPUT',
         TYPE_NUMBER   : 'NUMBER_INPUT',
         TYPE_PASSWORD : 'PASSWORD_INPUT',
+        TYPE_FILE     : 'FILE_INPUT',
 
         // event
         OPEN_EVENT    : 'OPEN_INPUT_LIGHTBOX'
