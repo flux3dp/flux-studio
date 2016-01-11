3d39ff9e23b491a354ff63081bf9d1fe96c984f1
Norman <nanron0919@gmail.com>
2016-01-11 15:50:38 +0800

    see #1108, if target is not a input element then disable default event

--- public/js/app/actions/global.js
+++ public/js/app/actions/global.js
@@ -11,7 +11,7 @@ define([
     // prevent delete (back) behavior
     var defaultKeyBehavior = function() {
         shortcuts.on(['BACK'], function(e) {
-            if ('BODY' === e.target.tagName) {
+            if ('INPUT' !== e.target.tagName) {
                 e.preventDefault();
             }
         });
