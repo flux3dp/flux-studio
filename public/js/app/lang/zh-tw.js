1a249ccd5d827d7941332c62ca56b64d04827a49
Norman <nanron0919@gmail.com>
2016-01-15 11:07:11 +0800

    see #1134, update sentences

--- public/js/app/lang/zh-tw.js
+++ public/js/app/lang/zh-tw.js
@@ -27,7 +27,7 @@ define(function() {
                 },
                 confirm: '確認更新',
                 upload_file: '請上傳檔案',
-                update_success: '更新成功',
+                update_success: '韌體更新上傳成功',
                 update_fail: '更新失敗'
             },
             software: {
