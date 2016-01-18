e0e52a986157079bf3d4c39883486daec94b0bda
Norman <nanron0919@gmail.com>
2016-01-18 12:45:42 +0800

    see #1115, create task for unit test

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    webserver = require('gulp-webserver');
+    webserver = require('gulp-webserver'),
+    mocha = require('gulp-mocha');
 
 gulp.task('sass', function () {
     gulp.src('./public/sass/**/*.scss')
@@ -26,3 +27,19 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('dev', ['sass:watch', 'webserver']);
+
+gulp.task('test', function() {
+    return gulp.
+        src('./_test/**/*.js', { read: false }).
+        pipe(mocha({
+            require: [
+                process.cwd() + '/_test/bootstrap.js'
+            ]
+        })).
+        once('error', function() {
+            process.exit(1);    // bad happens
+        }).
+        once('end', function() {
+            process.exit(); // good
+        });
+});
