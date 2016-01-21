10912aa043f34e1018ab8fed42b37cf372c572e6
Jim Kang <proclaim@gmail.com>
2016-01-21 15:46:43 +0800

    [1143] added duration formatter

---
+++ public/js/helpers/duration-formatter.js
@@ -0,0 +1,36 @@
+define([
+    'helpers/i18n',
+], function(
+    i18n
+) {
+    'use strict';
+    
+    var lang = i18n.get(),
+        oneHour = 3600,
+        oneMinute = 60;
+
+    return function(lengthInSecond) {
+
+        if(lengthInSecond >= oneHour) {
+
+            var hours = parseInt(lengthInSecond / oneHour),
+                minutes = parseInt(lengthInSecond % oneHour / oneMinute);
+
+            return `${hours} ${lang.monitor.hour} ${minutes} ${lang.monitor.minute}`;
+
+        } else if (lengthInSecond >= oneMinute) {
+
+            var minutes = parseInt(lengthInSecond / oneMinute),
+                seconds = parseInt(lengthInSecond % oneMinute);
+
+            return `${minutes} ${lang.monitor.minute} ${seconds} ${lang.monitor.second}`;
+
+        } else {
+
+            return `${lengthInSecond} ${lang.monitor.second}`;
+
+        }
+
+    };
+
+});
