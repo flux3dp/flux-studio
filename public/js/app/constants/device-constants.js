b7c5fe10f903abc629c3cd7d5cd1218b1a54cada
Norman <nanron0919@gmail.com>
2016-02-04 16:01:59 +0800

    see #52, into a formal message

--- public/js/app/constants/device-constants.js
+++ public/js/app/constants/device-constants.js
@@ -41,7 +41,7 @@ define(function() {
         OCCUPIED        : 'OCCUPIED',
         SCANNING        : 'SCANNING',
         CALIBRATING     : 'CALIBRATING',
-        MONITOR_TOO_OLD : 'FLUXMONITOR VERSION IS TOO OLD',
+        MONITOR_TOO_OLD : 'FLUXMONITOR_VERSION_IS_TOO_OLD',
 
         // folder
         NOT_EXIST       : 'NOT_EXIST',
