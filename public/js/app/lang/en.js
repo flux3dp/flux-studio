2f7ef28f104293ce0648b9f47a8db7d6217f6053
Norman <nanron0919@gmail.com>
2016-03-01 15:07:35 +0800

    see #92, define error of API connection

--- public/js/app/lang/en.js
+++ public/js/app/lang/en.js
@@ -922,7 +922,7 @@ define(function() {
                 caption: 'Firmware Outdated',
                 content: 'Please install the latest firmware with <a target="_blank" href="http://helpcenter.flux3dp.com/hc/en-us/articles/216251077">this guide</a>.'
             },
-            cant_establish_connection: 'Can not establish connection to FLUX api'
+            cant_establish_connection: 'Unable to initiate FLUX API, kindly use Help > Bug Report to export debug message and <a href="https://flux3dp.zendesk.com/hc/en-us/requests/new" target="_blank">contact FLUX support.</a>'
         },
         machine_status: {
             '-10': 'Raw mode',
