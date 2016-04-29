12a5e0bd1fce1fe4c680cbae5cda484c296e2929
Jim Kang <proclaim@gmail.com>
2016-04-29 15:32:36 +0800

    test commit fix #issue_number 1227

--- public/js/app/views/print/Monitor.jsx
+++ public/js/app/views/print/Monitor.jsx
@@ -503,7 +503,7 @@ define([
         _handleSelectFolder: function(pathName) {
             this._clearSelectedItem();
             var dir = this.state.currentDirectoryFolders;
-            // if it's a directory
+            // if it is a directory
             if(dir.some(function(d) {
                 return d === pathName;
             })) {
