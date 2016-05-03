ee7f01e6a5c3d1ccd8a352d08660cd87c2fc463b
Norman <nanron0919@gmail.com>
2016-05-03 15:24:05 +0800

    see #175, add chinese version

--- public/js/app/pages/Printer-Not-Found.jsx
+++ public/js/app/pages/Printer-Not-Found.jsx
@@ -73,7 +73,7 @@ define([
                     imgSrc = (
                         'en' === i18n.getActiveLang() ?
                         '/img/wifi-error-notify-en.png' :
-                        '/img/wifi-error-notify-en.png'
+                        '/img/wifi-error-notify-zh.png'
                     ),
                     content = (
                         <div className="device-not-found text-center">
