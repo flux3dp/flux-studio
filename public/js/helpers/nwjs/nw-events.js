08b6facf301e2d39b8e83c31c63f7b4645af72c7
Norman <nanron0919@gmail.com>
2016-05-09 18:06:05 +0800

    see #175, define close event for APP

--- public/js/helpers/nwjs/nw-events.js
+++ public/js/helpers/nwjs/nw-events.js
@@ -1,33 +1,48 @@
 /**
  * nwjs events
  */
-define(['helpers/i18n'], function(i18n) {
+define(['jquery', 'helpers/i18n'], function($, i18n) {
     'use strict';
 
-    if (true === window.FLUX.isNW) {
-        var win = nw.Window.get(),
-            lang = i18n.get(),
-            exec = requireNode('child_process').exec,
+    var win = nw.Window.get(),
+        lang = i18n.get();
+
+    window.FLUX.close = function() {
+        var exec = requireNode('child_process').exec,
+            $deferred = $.Deferred(),
             killCommmand;
 
+        switch (window.FLUX.osType) {
+        case 'osx':
+        case 'linux':
+            killCommmand = 'pkill -f flux_api';
+            break;
+        case 'win':
+            killCommmand = 'taskkill /F /FI "IMAGENAME eq flux_api.exe*"';
+            break;
+        }
+
+        exec(killCommmand, function(error, stdout, stderr) {
+            console.log(error, stdout, stderr);
+            if (error) {
+                $deferred.fail();
+            }
+            else {
+                $deferred.resolve();
+            }
+        });
+
+        return $deferred.promise();
+    };
+
+    if (true === window.FLUX.isNW) {
         win.title = 'FLUX Studio - {Version}';
 
         win.on('close', function() {
             if (true === window.confirm(lang.topmenu.sure_to_quit)) {
-                switch (window.FLUX.os) {
-                case 'osx':
-                case 'linux':
-                    killCommmand = 'pkill -f flux_api';
-                    break;
-                case 'win':
-                    killCommmand = 'taskkill /F /FI "IMAGENAME eq flux_api.exe*"';
-                    break;
-                }
-
-                exec(killCommmand, function(error, stdout, stderr) {
-                    console.log(error, stdout, stderr);
+                window.FLUX.close().always(function() {
+                    win.close(true);
                 });
-                win.close(true);
             }
         });
     }
