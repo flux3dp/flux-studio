1c5cc8642042cc9a7c5e824ca993b9019f854732
Jim Kang <proclaim@gmail.com>
2016-05-26 15:52:26 +0800

    added display timing for get path and draw path

--- public/js/app/actions/print.js
+++ public/js/app/actions/print.js
@@ -491,12 +491,12 @@ define([
 
         var processMetadata = function(m) {
             metadata = m;
-            var fcodeType = m.metadata.HEAD_TYPE
+            var fcodeType = m.metadata.HEAD_TYPE;
             if(fcodeType === 'EXTRUDER') {
                 fcodeConsole.getPath().then(processPath);
             }
             else {
-                var message = fcodeType === 'LASER' ? lang.message.fcodeForLaser : lang.message.fcodeForPen
+                var message = fcodeType === 'LASER' ? lang.message.fcodeForLaser : lang.message.fcodeForPen;
                 ProgressActions.close();
                 importFromFCode = false;
                 importFromGCode = false;
@@ -2420,14 +2420,19 @@ define([
     }
 
     function _handleSliceComplete() {
+        console.log('complete');
         if(previewMode) {
             slicingStatus.isComplete = true;
             _showWait(lang.print.drawingPreview, !showStopButton);
             slicingStatus.canInterrupt = false;
+            console.time('getPath');
             slicer.getPath().then(function(result) {
+                console.timeEnd('getPath');
                 slicingStatus.canInterrupt = true;
                 printPath = result;
+                console.time('drawPath');
                 _drawPath().then(function() {
+                    console.timeEnd('drawPath');
                     ProgressActions.close();
                     slicingStatus.showProgress = false;
                 });
