f6bcee88b620deaadbe0020450f839661cd15391
Jim Kang <proclaim@gmail.com>
2016-06-21 16:58:07 +0800

    update control command: resume, abort, pause with 'play resume', 'play abort' and 'play pause' respectively

--- public/js/helpers/api/control.js
+++ public/js/helpers/api/control.js
@@ -295,13 +295,13 @@ define([
 
                 return {
                     pause: function() {
-                        interrupt('pause');
+                        interrupt('play pause');
                     },
                     resume: function() {
-                        interrupt('resume');
+                        interrupt('play resume');
                     },
                     abort: function() {
-                        interrupt('abort');
+                        interrupt('play abort');
                     }
                 };
             },
@@ -387,8 +387,8 @@ define([
                     d.resolve(result);
                 };
 
-                ws.send('abort');
-                lastOrder = 'abort';
+                ws.send('play abort');
+                lastOrder = 'play abort';
 
                 return d.promise();
             },
@@ -417,8 +417,8 @@ define([
                     d.resolve(result);
                 };
 
-                ws.send('pause');
-                lastOrder = 'pause';
+                ws.send('play pause');
+                lastOrder = 'play pause';
 
                 return d.promise();
             },
@@ -432,8 +432,8 @@ define([
                     d.resolve(result);
                 };
 
-                ws.send('resume');
-                lastOrder = 'resume';
+                ws.send('play resume');
+                lastOrder = 'play resume';
 
                 return d.promise();
             },
