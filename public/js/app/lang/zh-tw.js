6e257f0afa5ba24940c12f640cc4067900f5b242
simonxeko <simonxeko@gmail.com>
2017-02-25 00:52:38 +0800

    Fix word display

--- public/js/app/lang/zh-tw.js
+++ public/js/app/lang/zh-tw.js
@@ -778,6 +778,7 @@ define(function() {
                 load_preset_title: '載入',
                 background: '自訂背景',
                 removeBackground: '移除背景',
+                load_calibrate_image: '載入校正圖片',
                 apply: '套用',
                 cancel: '取消',
                 save: '儲存'
