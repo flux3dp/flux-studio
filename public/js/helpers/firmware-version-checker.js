dda44631683e574e7f5b60818ac492d3497c7c35
Jim Kang <proclaim@gmail.com>
2017-03-02 16:50:46 +0800

    1. update version checker regex 2. code refine

--- public/js/helpers/firmware-version-checker.js
+++ public/js/helpers/firmware-version-checker.js
@@ -7,8 +7,7 @@ define([
     VersionChecker,
     DeviceMaster
 ) {
-    return function(device, version) {
-
+    const check = (device, key) => {
         let d = $.Deferred();
 
         DeviceMaster.selectDevice(device).then(() => {
@@ -16,9 +15,19 @@ define([
         })
         .then(deviceInfo => {
             let vc = VersionChecker(deviceInfo.version);
-            d.resolve(vc.meetVersion(version));
+            d.resolve(vc.meetVersion(requirement[key]));
         });
 
         return d.promise();
+    };
+
+    const requirement = {
+        BACKLASH                    : '1.5b12',
+        OPERATE_DURING_PAUSE        : '1.6.20',
+        UPGRADE_KIT_PROFILE_SETTING : '1.6.20'
+    };
+
+    return {
+        check
     };
 });
