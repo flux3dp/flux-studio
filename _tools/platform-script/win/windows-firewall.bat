6999e5ed1d377f801eaa14a9b0f3e9fbfe9963e1
simonxeko <simonxeko@gmail.com>
2017-03-07 19:32:29 +0800

    Driver Fix, final

--- _tools/platform-script/win/windows-firewall.bat
+++ _tools/platform-script/win/windows-firewall.bat
@@ -34,9 +34,9 @@ if %OS%==32BIT (
 ".\%FILE_NAME%" /install /q /norestart
 
 if %OS%==32BIT (
-   set FILE_NAME=dpinst_x86.exe /Q
+   set FILE_NAME=dpinst_x86.exe
 ) else (
-   set FILE_NAME=dpinst_x64.exe /Q
+   set FILE_NAME=dpinst_x64.exe
 )
 
-".\%FILE_NAME%"
+".\%FILE_NAME%" /S
