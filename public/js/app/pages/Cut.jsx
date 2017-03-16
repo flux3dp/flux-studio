1953f31e633e94cfe2a90503d0ad861c61dae8fd
simonxeko <simonxeko@gmail.com>
2017-03-16 22:59:01 +0800

    Basic Cutter Interface

--- public/js/app/pages/Cut.jsx
+++ public/js/app/pages/Cut.jsx
@@ -1,14 +1,22 @@
 define([
     'jquery',
     'react',
-    'jsx!views/holder/Setup-Panel',
-    'jsx!pages/Holder'
+    'jsx!views/cutter/Setup-Panel',
+    'jsx!pages/Holder',
+    'helpers/api/config',
+    'helpers/i18n',
 ], function(
     $,
     React,
     HolderSetupPanel,
-    HolderGenerator
+    HolderGenerator,
+    ConfigHelper,
+    i18n
 ) {
+
+    let Config = ConfigHelper(),
+        lang = i18n.lang;
+
     'use strict';
     
     return function(args) {
@@ -23,12 +31,47 @@ define([
                     };
                 },
 
-                renderSetupPanel: function(holder) {
+                getInitialState: function() {
+                    return {
+                        options: {
+                            zOffset: 0.1,
+                            overcut: 2,
+                            speed: 20,
+                            bladeRadius: 0.24
+                        }
+                    };
+                },
+
+                componentDidMount: function() {
+                    let options = Config.read('cut-defaults') || {};
+                    options = {
+                        zOffset: options.zOffset || 0.1,
+                        overcut: options.overcut || 2,
+                        speed: options.speed || 10,
+                        bladeRadius: options.bladeRadius || 0.24,
+                    };
+                    if (!Config.read('cut-defaults')) {
+                        Config.write('cut-defaults', options);
+                    }
+                    this.setState({options});
+                },
+
+                _fetchFormalSettings: function() {
+                    let options = Config.read('cut-defaults') || {};
+                    return {
+                        zOffset: options.zOffset || 0.1,
+                        overcut: options.overcut || 2,
+                        speed: options.speed || 10,
+                        bladeRadius: options.bladeRadius || 0.24,
+                    };
+                },
+
+                _renderSetupPanel: function(holder) {
                     return <HolderSetupPanel
                         page={holder.props.page}
                         className="operating-panel"
                         imageFormat={holder.state.fileFormat}
-                        defaults={holder.state.setupPanelDefaults}
+                        defaults={holder.state.panelOptions}
                         ref="setupPanel"
                     />;
                 },
@@ -37,7 +80,13 @@ define([
                     console.log('Load Holder', Holder);
                     // return <div />;
                     
-                    return <Holder page={this.props.page} renderSetupPanel={this.renderSetupPanel} />;
+                    return <Holder
+                        page={this.props.page}
+                        acceptFormat={'image/svg'}
+                        panelOptions={this.state.options}
+                        fetchFormalSettings={this._fetchFormalSettings}
+                        renderSetupPanel={this._renderSetupPanel}
+                    />;
                 }
         });
 
---
+++ public/js/app/views/cutter/Setup-Panel.jsx
@@ -0,0 +1,206 @@
+define([
+    'jquery',
+    'react',
+    'jsx!widgets/Select',
+    'jsx!widgets/List',
+    'jsx!widgets/Modal',
+    'jsx!views/laser/Advanced-Panel',
+    'jsx!widgets/Text-Toggle',
+    'jsx!widgets/Unit-Input',
+    'jsx!widgets/Button-Group',
+    'jsx!widgets/Alert',
+    'jsx!widgets/Dialog-Menu',
+    'helpers/api/config',
+    'helpers/i18n',
+    'helpers/round',
+    'plugins/classnames/index'
+], function(
+    $,
+    React,
+    SelectView,
+    List,
+    Modal,
+    AdvancedPanel,
+    TextToggle,
+    UnitInput,
+    ButtonGroup,
+    Alert,
+    DialogMenu,
+    ConfigHelper,
+    i18n,
+    round,
+    ClassNames
+) {
+    'use strict';
+
+    let Config = ConfigHelper(),
+        lang = i18n.lang;
+
+    return React.createClass({
+
+        getDefaultProps: function() {
+            return {
+                defaults: {},
+                imageFormat: 'svg'  // svg, bitmap
+            };
+        },
+
+        getInitialState: function() {
+            return {
+                defaults: this.props.defaults
+            };
+        },
+
+        isShading: function() {
+            return false;
+        },
+
+        // UI Events
+        _saveLastestSet: function() {
+            var self = this,
+                refs = self.refs,
+                opts = {
+                    zOffset: refs.zOffset.value(),
+                    overcut: refs.overcut.value(),
+                    speed: refs.speed.value(),
+                    bladeRadius: refs.bladeRadius.value()
+                },
+                state = {
+                    defaults: opts
+                };
+
+            Config.write('cut-defaults', opts);
+
+            self.setState(state);
+        },
+
+        openSubPopup: function(e) {
+            this.refs.dialogMenu.toggleSubPopup(e);
+        },
+
+        _updateDefaults: function(e, value) {
+            this._saveLastestSet();
+            this.openSubPopup(e);
+        },
+
+        // Lifecycle
+        _renderZOffset: function() {
+            var min = -1;
+
+            return {
+                label: (
+                    <div title={lang.cut.zOffsetTip}>
+                        <span className="caption">{lang.cut.zOffset}</span>
+                        <span>{this.state.defaults.zOffset}</span>
+                        <span>{lang.draw.units.mm}</span>
+                    </div>
+                ),
+                content: (
+                    <div className="object-height-input">
+                        <UnitInput
+                            ref="zOffset"
+                            defaultUnit="mm"
+                            defaultValue={this.state.defaults.zOffset}
+                            getValue={this._updateDefaults}
+                            min={min}
+                            max={150}
+                        />
+                    </div>
+                )
+            };
+        },
+
+        _renderOvercut: function() {
+            var max = 50;
+
+            return {
+                label: (
+                    <div title={lang.cut.overcutTip}>
+                        <span className="caption">{lang.cut.overcut}</span>
+                        <span>{this.state.defaults.overcut}</span>
+                        <span>{lang.draw.units.mm}</span>
+                    </div>
+                ),
+                content: (
+                    <div className="object-height-input">
+                        <UnitInput
+                            ref="overcut"
+                            defaultUnit="mm"
+                            defaultValue={this.state.defaults.overcut}
+                            getValue={this._updateDefaults}
+                            min={0}
+                            max={max}
+                        />
+                    </div>
+                )
+            };
+        },
+
+        _renderSpeed: function() {
+            return {
+                label: (
+                    <div title={lang.cut.speedTip}>
+                        <span className="caption">{lang.cut.speed}</span>
+                        <span>{this.state.defaults.speed}</span>
+                        <span>{lang.draw.units.mms}</span>
+                    </div>
+                ),
+                content: (
+                    <div className="object-height-input">
+                        <UnitInput
+                            ref="speed"
+                            defaultUnit="mm/s"
+                            defaultUnitType="speed"
+                            defaultValue={this.state.defaults.speed}
+                            getValue={this._updateDefaults}
+                            min={0.8}
+                            max={150}
+                        />
+                    </div>
+                )
+            };
+        },
+
+         _renderBladeRadius: function() {
+            var min = Math.max(5, this.state.defaults.bladeRadius);
+
+            return {
+                label: (
+                    <div title={lang.cut.bladeRadiusTip}>
+                        <span className="caption">{lang.cut.bladeRadius}</span>
+                        <span>{this.state.defaults.bladeRadius}</span>
+                        <span>{lang.draw.units.mm}</span>
+                    </div>
+                ),
+                content: (
+                    <div className="object-height-input">
+                        <UnitInput
+                            ref="bladeRadius"
+                            defaultUnit="mm"
+                            defaultValue={this.state.defaults.bladeRadius}
+                            getValue={this._updateDefaults}
+                            min={min}
+                            max={150}
+                        />
+                    </div>
+                )
+            };
+        },
+
+        render: function() {
+            let items = [
+                    this._renderZOffset(),
+                    this._renderOvercut(),
+                    this._renderSpeed(),
+                    this._renderBladeRadius()
+                ];
+
+            return (
+                <div className="setup-panel operating-panel">
+                    <DialogMenu ref="dialogMenu" items={items}/>
+                </div>
+            );
+        }
+
+    });
+});
