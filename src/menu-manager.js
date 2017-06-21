21b219ab4fc728eb7c515dcd113c982fe757dcfd
yagami.cerberus <yagami-cerberus@users.noreply.github.com>
2017-06-21 18:04:48 +0800

    say cheese

--- src/menu-manager.js
+++ src/menu-manager.js
@@ -317,7 +317,7 @@ class MenuManager extends EventEmitter {
         let menuId = getDeviceMenuId(uuid, data);
         this._device_list[menuId] = data;
 
-        var labelName = data.source === "h2h" ? `${menuitem.label} (USB)` : menuitem.label;
+        var labelName = data.source === "h2h" ? `${data.name} (USB)` : data.name;
         for(let menuitem of this._deviceMenu.submenu.items) {
             if(menuitem.id === menuId && menuitem.label !== data.name) {
                 menuitem.label = data.name;
