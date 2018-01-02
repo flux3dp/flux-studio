bd02d8114fe443abec0e2766fe47da5b9e75f91a
Jeff Chen <jeffontheground@gmail.com>
2018-01-02 14:23:32 +0800

    format beambox.jsx

--- public/js/app/pages/Beambox.jsx
+++ public/js/app/pages/Beambox.jsx
@@ -14,7 +14,7 @@ define([
     'jsx!views/beambox/Object-Panels-Controller',
     'jsx!views/beambox/Right-Panels/Laser-Panel-Controller',
     'app/actions/global-interaction', //need to include to run this
-], function(
+], function (
     React,
     beamboxEvents,
     DeviceConstants,
@@ -30,176 +30,176 @@ define([
     ObjectPanelsController,
     LaserPanelController
 ) {
-    'use strict';
+        'use strict';
 
-    const Config = ConfigHelper();
-    const lang = i18n.lang;
-    const machineCommand = {
-        START: 'START'
-    };
+        const Config = ConfigHelper();
+        const lang = i18n.lang;
+        const machineCommand = {
+            START: 'START'
+        };
 
-    (function(){
-        //init config
-        const customConfig = Config.read('beambox-preference');
-        const updatedConfig = $.extend({}, DefaultConfig, customConfig);
-        Config.write('beambox-preference', updatedConfig);
-    })();
+        (function () {
+            //init config
+            const customConfig = Config.read('beambox-preference');
+            const updatedConfig = $.extend({}, DefaultConfig, customConfig);
+            Config.write('beambox-preference', updatedConfig);
+        })();
 
-    ObjectPanelsController.init("object-panels-placeholder");
-    LaserPanelController.init("layer-laser-panel-placeholder");
+        ObjectPanelsController.init("object-panels-placeholder");
+        LaserPanelController.init("layer-laser-panel-placeholder");
 
 
-    if(!Config.read('beambox-preference')['mouse-input-device']) {
-        function chooseIsTouchpad() {
-            AlertActions.showPopupCustomGroup(
-                'confirm_mouse_input_device',
-                lang.beambox.popup.select_favor_input_device,
-                [lang.beambox.popup.touchpad, lang.beambox.popup.mouse],
-                '',
-                '',
-                [
-                  () => {
-                        Config.update('beambox-preference', 'mouse-input-device', 'TOUCHPAD');
-                  },
-                  () => {
-                        Config.update('beambox-preference', 'mouse-input-device', 'MOUSE');
-                  },
-                ]
-            );
+        if (!Config.read('beambox-preference')['mouse-input-device']) {
+            function chooseIsTouchpad() {
+                AlertActions.showPopupCustomGroup(
+                    'confirm_mouse_input_device',
+                    lang.beambox.popup.select_favor_input_device,
+                    [lang.beambox.popup.touchpad, lang.beambox.popup.mouse],
+                    '',
+                    '',
+                    [
+                        () => {
+                            Config.update('beambox-preference', 'mouse-input-device', 'TOUCHPAD');
+                        },
+                        () => {
+                            Config.update('beambox-preference', 'mouse-input-device', 'MOUSE');
+                        },
+                    ]
+                );
+            }
+            chooseIsTouchpad();
         }
-        chooseIsTouchpad();        
-    }
 
-    return function(args = {}) {
-        let Svg = SvgGenerator(args);
+        return function (args = {}) {
+            let Svg = SvgGenerator(args);
             self = this;
 
-        class view extends React.Component {
-            constructor() {
-                super();
-                this.beamboxEvents = beamboxEvents.call(this); //to let beamboxEvents know this.state.selectedPrinter
-                this.state = {
-                    openPrinterSelectorWindow: false
-                };
-            }
+            class view extends React.Component {
+                constructor() {
+                    super();
+                    this.beamboxEvents = beamboxEvents.call(this); //to let beamboxEvents know this.state.selectedPrinter
+                    this.state = {
+                        openPrinterSelectorWindow: false
+                    };
+                }
 
-            _fetchFormalSettings(holder) {
-                const options = Config.read('beambox-preference');
-                //   const max = options['max-strength'];
-                //   max = lang.laser.advanced.form.power.max;
-                return {
-                    //object_height: options.objectHeight,
-                    //height_offset: options.heightOffset || 0,
-                    //laser_speed: options.material.data.laser_speed,
-                    //calibration: 0,
-                    //power: options.material.data.power / max,
-                };
-            }
+                _fetchFormalSettings(holder) {
+                    const options = Config.read('beambox-preference');
+                    //   const max = options['max-strength'];
+                    //   max = lang.laser.advanced.form.power.max;
+                    return {
+                        //object_height: options.objectHeight,
+                        //height_offset: options.heightOffset || 0,
+                        //laser_speed: options.material.data.laser_speed,
+                        //calibration: 0,
+                        //power: options.material.data.power / max,
+                    };
+                }
 
-            _handleExportClick(filemode) {
-                this.beamboxEvents.exportTaskCode(this._fetchFormalSettings(), filemode);
-            }
+                _handleExportClick(filemode) {
+                    this.beamboxEvents.exportTaskCode(this._fetchFormalSettings(), filemode);
+                }
 
-            _handleStartClick() {
-                this.setState({
-                    openPrinterSelectorWindow: true,
-                    machineCommand: machineCommand.START,
-                    settings: this._fetchFormalSettings(),
-                    printerSelectorWindowStyle: {}
-                });
-            }
+                _handleStartClick() {
+                    this.setState({
+                        openPrinterSelectorWindow: true,
+                        machineCommand: machineCommand.START,
+                        settings: this._fetchFormalSettings(),
+                        printerSelectorWindowStyle: {}
+                    });
+                }
 
-            _renderActionButtons() {
-                //globalInteraction.onImageChanged(this.state.hasImage);
-                var cx = React.addons.classSet,
-                    buttons = [{
-                        label: lang.laser.get_fcode,
-                        className: cx({
-                            'btn-disabled': false,
-                            'btn-default': true,
-                            'btn-hexagon': true,
-                            'btn-get-fcode': true
-                        }),
-                        dataAttrs: {
-                            'ga-event': 'get-laser-fcode'
-                        },
-                        onClick: this._handleExportClick.bind(this, '-f')
-                    }, {
-                        label: lang.monitor.start,
-                        className: cx({
-                            'btn-disabled': false,
-                            'btn-default': true,
-                            'btn-hexagon': true,
-                            'btn-go': true
-                        }),
-                        dataAttrs: {
-                            'ga-event': 'laser-goto-monitor'
-                        },
-                        onClick: this._handleStartClick.bind(this)
-                    }];
+                _renderActionButtons() {
+                    //globalInteraction.onImageChanged(this.state.hasImage);
+                    var cx = React.addons.classSet,
+                        buttons = [{
+                            label: lang.laser.get_fcode,
+                            className: cx({
+                                'btn-disabled': false,
+                                'btn-default': true,
+                                'btn-hexagon': true,
+                                'btn-get-fcode': true
+                            }),
+                            dataAttrs: {
+                                'ga-event': 'get-laser-fcode'
+                            },
+                            onClick: this._handleExportClick.bind(this, '-f')
+                        }, {
+                            label: lang.monitor.start,
+                            className: cx({
+                                'btn-disabled': false,
+                                'btn-default': true,
+                                'btn-hexagon': true,
+                                'btn-go': true
+                            }),
+                            dataAttrs: {
+                                'ga-event': 'laser-goto-monitor'
+                            },
+                            onClick: this._handleStartClick.bind(this)
+                        }];
 
-                return (
-                    <ButtonGroup buttons={buttons} className="beehive-buttons action-buttons" />
-                );
-            }
-
-            _renderLeftPanel() {
-                return (<LeftPanel />);
-            }
-
-            _renderPrinterSelectorWindow() {
-                if (!this.state.openPrinterSelectorWindow) { return ''; }
-                var self = this,
-                    onGettingPrinter = function(auth_printer) {
-                        self.setState({
-                            selectedPrinter: auth_printer,
-                            openPrinterSelectorWindow: false
-                        });
-
-                        if (self.state.machineCommand === machineCommand.START) {
-                            self.beamboxEvents.uploadFcode(self._fetchFormalSettings());
-                        };
-                    },
-                    onClose = function(e) {
-                        self.setState({
-                            openPrinterSelectorWindow: false
-                        });
-                    },
-                    content = (
-                        <PrinterSelector
-                            uniqleId="laser"
-                            className="laser-device-selection-popup"
-                            lang={lang}
-                            onClose={onClose}
-                            onGettingPrinter={onGettingPrinter}
-                            WindowStyle={this.state.printerSelectorWindowStyle}
-                        />
+                    return (
+                        <ButtonGroup buttons={buttons} className="beehive-buttons action-buttons" />
                     );
-                return (
-                    <Modal content={content} onClose={onClose}/>
-                );
+                }
+
+                _renderLeftPanel() {
+                    return (<LeftPanel />);
+                }
+
+                _renderPrinterSelectorWindow() {
+                    if (!this.state.openPrinterSelectorWindow) { return ''; }
+                    var self = this,
+                        onGettingPrinter = function (auth_printer) {
+                            self.setState({
+                                selectedPrinter: auth_printer,
+                                openPrinterSelectorWindow: false
+                            });
+
+                            if (self.state.machineCommand === machineCommand.START) {
+                                self.beamboxEvents.uploadFcode(self._fetchFormalSettings());
+                            };
+                        },
+                        onClose = function (e) {
+                            self.setState({
+                                openPrinterSelectorWindow: false
+                            });
+                        },
+                        content = (
+                            <PrinterSelector
+                                uniqleId="laser"
+                                className="laser-device-selection-popup"
+                                lang={lang}
+                                onClose={onClose}
+                                onGettingPrinter={onGettingPrinter}
+                                WindowStyle={this.state.printerSelectorWindowStyle}
+                            />
+                        );
+                    return (
+                        <Modal content={content} onClose={onClose} />
+                    );
+                }
+
+
+                render() {
+
+                    var actionButtons = this._renderActionButtons(),
+                        leftPanel = this._renderLeftPanel(),
+                        printerSelector = this._renderPrinterSelectorWindow();
+
+                    return (
+                        <div className="studio-container beambox-studio">
+                            <div id="grid_mask"></div>
+                            {leftPanel}
+                            <Svg />
+                            {actionButtons}
+                            {printerSelector}
+                            <div id="object-panels-placeholder"></div>
+                        </div>
+                    );
+                }
             }
 
-        
-          render() {
-
-            var actionButtons = this._renderActionButtons(),
-                leftPanel = this._renderLeftPanel(),
-                printerSelector = this._renderPrinterSelectorWindow();
-
-            return (
-                    <div className="studio-container beambox-studio">
-                        <div id="grid_mask"></div>
-                        {leftPanel}
-                        <Svg />
-                        {actionButtons}
-                        {printerSelector}
-                        <div id="object-panels-placeholder"></div>
-                    </div>
-            );
-          }
-        }
-
-        return view;
-    };
-});
+            return view;
+        };
+    });
