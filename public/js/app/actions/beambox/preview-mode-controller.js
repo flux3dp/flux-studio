dbb52cac4e6ee0ed49472cc4079906f8f10d52b0
Jeff Chen <jeffontheground@gmail.com>
2018-02-01 18:02:36 +0800

    Add clear preview button

--- public/js/app/actions/beambox/preview-mode-controller.js
+++ public/js/app/actions/beambox/preview-mode-controller.js
@@ -9,7 +9,7 @@ define([
     'helpers/api/fcode-reader',
     'app/actions/alert-actions',
     'app/actions/global-actions',
-    'helpers/sprintf',    
+    'helpers/sprintf',
     'helpers/check-device-status',
     'app/actions/beambox/constant'
 ], function (
@@ -27,7 +27,6 @@ define([
     checkDeviceStatus,
     Constant
 ) {
-    'use strict';
     const LANG = i18n.lang.beambox.left_panel;
     class PreviewModeController {
         constructor() {
@@ -37,6 +36,7 @@ define([
             this.cameraStream = null;
             this.cameraOffset = null;
             this.canvas = document.createElement('canvas');
+            this.cameraCanvasUrl = '';
             this.errorCallback = function(){};
 
             this.canvas.width = Constant.dimension.width;
@@ -45,12 +45,12 @@ define([
         }
 
         //main functions
-        
+
         async start(selectedPrinter, errCallback) {
             await this._reset();
-            
+
             await DeviceMaster.select(selectedPrinter);
-            
+
             ProgressActions.open(ProgressConstants.NONSTOP, sprintf(i18n.lang.message.connectingMachine, selectedPrinter.name));
 
             try {
@@ -65,7 +65,7 @@ define([
             } catch (error) {
                 throw error;
             } finally {
-                ProgressActions.close();                
+                ProgressActions.close();
             }
         }
 
@@ -78,25 +78,28 @@ define([
         }
 
         preview(x, y) {
-            if(this.isPreviewBlocked) return;
+            if (this.isPreviewBlocked) {
+                return;
+            }
             this.isPreviewBlocked = true;
             const constrainedXY = this._constrainPreviewXY(x, y);
             x = constrainedXY.x;
             y = constrainedXY.y;
-            
-            $(workarea).css('cursor', 'wait');      
+
+            $(workarea).css('cursor', 'wait');
 
             this._getPhotoAfterMove(x, y)
-            .then((imgUrl)=>{
-                $(workarea).css('cursor', 'url(img/camera-cursor.svg), cell');                
-                this._drawIntoBackground(imgUrl, x, y);
-                this.isPreviewBlocked = false;
-            })
-            .catch((error)=>{
-                console.log(error);
-                this.errorCallback(error.message);
-                this.isPreviewBlocked = false;
-            });
+                .then((imgUrl)=>{
+                    $(workarea).css('cursor', 'url(img/camera-cursor.svg), cell');
+                    this._drawIntoBackground(imgUrl, x, y);
+                    this.isPreviewBlocked = false;
+                    $('.left-panel .preview-btns .clear-preview').show();// bad practice ~~~~
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    this.errorCallback(error.message);
+                    this.isPreviewBlocked = false;
+                });
         }
 
         // x, y in mm
@@ -104,11 +107,29 @@ define([
             return this._getPhotoAfterMoveTo(movementX, movementY);
         }
 
+        clearGraffiti() {
+            window.svgCanvas.setBackground('#fff');
+
+            // clear canvas
+            this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+            // reset cameraCanvasUrl
+            URL.revokeObjectURL(this.cameraCanvasUrl);
+            this.cameraCanvasUrl = '';
+
+            // hide 'x' button
+            $('.left-panel .preview-btns .clear-preview').hide();// bad practice ~~~~
+        }
+
         isPreviewMode() {
             return this.isPreviewModeOn;
         }
 
+        isGraffitied() {
+            return this.cameraCanvasUrl === '';
+        }
 
+        //helper functions
 
         async _retrieveCameraOffset() {
             try {
@@ -123,21 +144,21 @@ define([
                 y:          Number(/Y:\s?(\-?\d+\.?\d+)/.exec(resp.value)[1]),
                 angle:      Number(/R:\s?(\-?\d+\.?\d+)/.exec(resp.value)[1]),
                 scaleRatio: Number(/S:\s?(\-?\d+\.?\d+)/.exec(resp.value)[1])
-            }
+            };
             if ((this.cameraOffset.x === 0) && (this.cameraOffset.y === 0)) {
                 this.cameraOffset = {
                     x: Constant.camera.offsetX_ideal,
                     y: Constant.camera.offsetY_ideal,
                     angle: 0,
                     scaleRatio: Constant.camera.scaleRatio_ideal,
-                }
+                };
             }
         }
 
         _getCameraOffset() {
             return this.cameraOffset;
         }
-        
+
         _clearData() {
             this.storedPrinter = null;
             this.isPreviewModeOn = false;
@@ -199,13 +220,14 @@ define([
             return imgUrl;
         }
 
+        //just fot _getPhotoAfterMoveTo()
         _getPhotoFromStream() {
             const d = $.Deferred();
-            
+
             const waitTimeForMovementStop = Constant.camera.waitTimeForMovementStop; //millisecond. this value need optimized
             setTimeout(() => {
                 this.cameraStream.take(1).subscribe((imageBlob) => {
-                    const imgUrl = URL.createObjectURL(imageBlob); 
+                    const imgUrl = URL.createObjectURL(imageBlob);
                     d.resolve(imgUrl);
                 });
             }, waitTimeForMovementStop);
@@ -213,37 +235,14 @@ define([
             return d.promise();
         }
 
-        _cropAndRotateImg(imageObj) {
-            const angle = this._getCameraOffset().angle;
-            const scaleRatio = this._getCameraOffset().scaleRatio;
-
-            const cvs = document.createElement("canvas");
-            const ctx = cvs.getContext("2d");
-            
-            const a = angle;
-            const s = scaleRatio;
-            const w = imageObj.width;
-            const h = imageObj.height;
-
-            const c = h / (Math.cos(a) + Math.sin(a));
-            const dstx = (h - w) / 2 * s;
-            const dsty = - h * Math.sin(a) / (Math.cos(a) + Math.sin(a)) * s;
-        
-            cvs.width = cvs.height = c * s;
-        
-            ctx.rotate(a);
-            ctx.drawImage(imageObj, 0, 0, w, h, dstx, dsty, w * s, h * s);
-
-            return cvs;
-        }
-
+        //just for preview()
         _drawIntoBackground(imgUrl, x, y) {
             const img = new Image();
             img.src = imgUrl;
             img.onload = () => {
                 // free unused blob memory
                 URL.revokeObjectURL(imgUrl);
-                
+
                 const img_regulated = this._cropAndRotateImg(img);
 
                 const dstX = x - img_regulated.width/2;
@@ -260,16 +259,45 @@ define([
             };
         }
 
+        //just for _drawIntoBackground()
+        _cropAndRotateImg(imageObj) {
+            const angle = this._getCameraOffset().angle;
+            const scaleRatio = this._getCameraOffset().scaleRatio;
+
+            const cvs = document.createElement('canvas');
+            const ctx = cvs.getContext('2d');
+
+            const a = angle;
+            const s = scaleRatio;
+            const w = imageObj.width;
+            const h = imageObj.height;
+
+            const c = h / (Math.cos(a) + Math.sin(a));
+            const dstx = (h - w) / 2 * s;
+            const dsty = - h * Math.sin(a) / (Math.cos(a) + Math.sin(a)) * s;
+
+            cvs.width = cvs.height = c * s;
+
+            ctx.rotate(a);
+            ctx.drawImage(imageObj, 0, 0, w, h, dstx, dsty, w * s, h * s);
+
+            return cvs;
+        }
+
+        //just for end()
         _clearBoundary() {
             const canvasBackground = svgedit.utilities.getElem('canvasBackground');
             const previewBoundary = svgedit.utilities.getElem('previewBoundary');
             canvasBackground.removeChild(previewBoundary);
         }
+
+        // just for start()
         _drawBoundary() {
             const canvasBackground = svgedit.utilities.getElem('canvasBackground');
             const previewBoundary = this._getPreviewBoundary();
-			canvasBackground.appendChild(previewBoundary);
+            canvasBackground.appendChild(previewBoundary);
         }
+        // just for _drawBoundary()
         _getPreviewBoundary() {
             const previewBoundaryId = 'previewBoundary';
             const color = 'rgba(200,200,200,0.8)';
@@ -287,7 +315,7 @@ define([
             });
             const uncapturabledHeight = (this._getCameraOffset().y * Constant.dpmm) - (Constant.camera.imgHeight * this._getCameraOffset().scaleRatio / 2);
             const uncapturabledHeightRatio = uncapturabledHeight / Constant.dimension.height;
-            
+
             const descText = svgCanvas.addSvgElementFromJson({
                 'element': 'text',
                 'attr': {
