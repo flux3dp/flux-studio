a7ec5242c531425e6501062643889f43146b1c31
Jeff Chen <jeffontheground@gmail.com>
2018-02-23 15:38:48 +0800

    Rename threshold to shadingThreshold

--- public/js/app/views/beambox/Object-Panels/Object-Panels.jsx
+++ public/js/app/views/beambox/Object-Panels/Object-Panels.jsx
@@ -8,69 +8,63 @@ define([
     'jsx!views/beambox/Object-Panels/EllipsePosition',
     'jsx!views/beambox/Object-Panels/EllipseRadius',
     'jsx!views/beambox/Object-Panels/Line',
-    'jsx!views/beambox/Object-Panels/Threshold'
+    'jsx!views/beambox/Object-Panels/ShadingThreshold'
 ], function(
     React,
     PropTypes,
     ClassNames,
-    PositionPanel, 
-    RotationPanel, 
-    SizePanel, 
-    EllipsePositionPanel, 
+    PositionPanel,
+    RotationPanel,
+    SizePanel,
+    EllipsePositionPanel,
     EllipseRadiusPanel,
     LinePanel,
-    ThresholdPanel 
+    ShadingThresholdPanel
 ) {
     'use strict';
 
-    const validPanels = {
+    const validPanelsMap = {
         'unknown':  [],
         'rect':     ['position', 'size', 'rotation'],
         'ellipse':  ['ellipsePosition', 'ellipseRadius', 'rotation'],
         'line':     ['line', 'rotation'],
-        'image':    ['position', 'size', 'rotation', 'threshold'],
+        'image':    ['position', 'size', 'rotation', 'shadingThreshold'],
         'text':     ['rotation'],
         'use':      ['position', 'size', 'rotation']
     };
 
-    function _getValidPanels(type) {
-        return validPanels[type]||validPanels['unknown'];
-    }
-
-
-    
     let ObjectPanel = React.createClass({
         propTypes: {
-            type: PropTypes.oneOf(Object.keys(validPanels)).isRequired,
+            type: PropTypes.oneOf(Object.keys(validPanelsMap)).isRequired,
             data: PropTypes.object.isRequired,
             $me: PropTypes.object.isRequired,
             isEditable: PropTypes.bool.isRequired
         },
-                  
-        
+
+
         _renderPanels: function() {
             const data = this.props.data;
             const type = this.props.type;
             const $me = this.props.$me;
 
-            const validPanels = _getValidPanels(this.props.type);
+            const validPanels = validPanelsMap[this.props.type] || validPanelsMap['unknown'];
             let panelsToBeRender = [];
             validPanels.forEach(function(panelName) {
                 let panel;
                 switch (panelName) {
-                    case 'position':        panel = <PositionPanel key={panelName} x={data.position.x} y={data.position.y} type={type}/>;       break;
-                    case 'rotation':        panel = <RotationPanel key={panelName} angle={data.rotation.angle} />;                  break;
-                    case 'size':            panel = <SizePanel key={panelName} width={data.size.width} height={data.size.height} type={type}/>;         break;
-                    case 'ellipsePosition': panel = <EllipsePositionPanel key={panelName} cx={data.ellipsePosition.cx} cy={data.ellipsePosition.cy}/>;  break;
-                    case 'ellipseRadius':   panel = <EllipseRadiusPanel key={panelName} rx={data.ellipseRadius.rx} ry={data.ellipseRadius.ry}/>;        break;
-                    case 'line':            panel = <LinePanel key={panelName} x1={data.line.x1} y1={data.line.y1} x2={data.line.x2} y2={data.line.y2}/>;       break;
-                    case 'threshold':       panel = <ThresholdPanel key={panelName} shading={data.image.shading} threshold={data.image.threshold} $me={$me}/>;  break;
+                    case 'position':            panel = <PositionPanel key={panelName} x={data.position.x} y={data.position.y} type={type}/>;       break;
+                    case 'rotation':            panel = <RotationPanel key={panelName} angle={data.rotation.angle} />;                  break;
+                    case 'size':                panel = <SizePanel key={panelName} width={data.size.width} height={data.size.height} type={type}/>;         break;
+                    case 'ellipsePosition':     panel = <EllipsePositionPanel key={panelName} cx={data.ellipsePosition.cx} cy={data.ellipsePosition.cy}/>;  break;
+                    case 'ellipseRadius':       panel = <EllipseRadiusPanel key={panelName} rx={data.ellipseRadius.rx} ry={data.ellipseRadius.ry}/>;        break;
+                    case 'line':                panel = <LinePanel key={panelName} x1={data.line.x1} y1={data.line.y1} x2={data.line.x2} y2={data.line.y2}/>;       break;
+                    case 'shadingThreshold':    panel = <ShadingThresholdPanel key={panelName} shading={data.image.shading} threshold={data.image.threshold} $me={$me}/>;  break;
                 }
                 panelsToBeRender.push(panel);
             });
             return panelsToBeRender;
         },
-        
+
         _findPositionStyle: function() {
             const angle = function(){
                 const A = $('#selectorGrip_resize_w').offset();
@@ -82,7 +76,7 @@ define([
                 if(degree<0) degree += 360;
                 return degree;
             }();
-            
+
             const thePoint = (function () {
                 const E = $('#selectorGrip_resize_e').offset();
                 const S = $('#selectorGrip_resize_s').offset();
@@ -132,7 +126,7 @@ define([
                 </div>
             );
         }
-        
+
     });
 
     return ObjectPanel;
