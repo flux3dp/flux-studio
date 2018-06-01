ee4ca87bec15de58c0238dad83895f77b2d41ad3
Jeff Chen <jeffontheground@gmail.com>
2018-06-02 00:42:55 +0800

    Fix firmware-version comparing different length problem

--- public/js/helpers/version-compare.js
+++ public/js/helpers/version-compare.js
@@ -1,13 +1,27 @@
 define(function () {
-    'use strict';
+    const padArrayWithZero = (arr, length) => {
+        while(arr.length < length) {
+            arr.push('0');
+        }
+        return arr;
+    };
 
     return function (currVer, promoteVer) {
-        currVer = currVer || "0.0.0";
-        promoteVer = promoteVer || "0.0.0";
-        if (currVer === promoteVer) return false;
-        let currVerArr = currVer.split(".");
-        let promoteVerArr = promoteVer.split(".");
+        currVer = currVer || '0.0.0';
+        promoteVer = promoteVer || '0.0.0';
+
+        if (currVer === promoteVer) {
+            return false;
+        }
+
+        let currVerArr = currVer.split('.');
+        let promoteVerArr = promoteVer.split('.');
+
         let len = Math.max(currVerArr.length, promoteVerArr.length);
+
+        currVerArr = padArrayWithZero(currVerArr, len);
+        promoteVerArr = padArrayWithZero(promoteVerArr, len);
+
         for (let i = 0; i < len; i++) {
             let proVal = promoteVerArr[i],
                 curVal = currVerArr[i];
@@ -19,4 +33,4 @@ define(function () {
         }
         return false;
     };
-})
+});
