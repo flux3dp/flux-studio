635c82a23ff4c175696fce51c516044257cd57cc
Jeff Chen <jeffontheground@gmail.com>
2018-06-21 14:23:35 +0800

    Fix left panel state

--- public/js/app/views/beambox/Left-Panels/Left-Panel.jsx
+++ public/js/app/views/beambox/Left-Panels/Left-Panel.jsx
@@ -1,20 +1,16 @@
 define([
     'react',
     'reactDOM',
-    'jsx!widgets/Dialog-Menu',
     'jsx!views/beambox/Left-Panels/Insert-Object-Submenu',
     'jsx!views/beambox/Left-Panels/Preview-Button',
     'jsx!views/beambox/Left-Panels/Advanced-Panel',
-    'app/actions/beambox/svgeditor-function-wrapper',
     'helpers/i18n',
 ], function(
     React,
     ReactDOM,
-    DialogMenu,
     InsertObjectSubmenu,
     PreviewButton,
     AdvancedPanel,
-    FnWrapper,
     i18n
 ) {
     const LANG = i18n.lang.beambox.left_panel;
@@ -27,6 +23,11 @@ define([
                 isAdvancedPanelOpen: false
                 // preview button is managed by itself
             };
+        }
+        componentDidMount() {
+            $('#svgcanvas').mouseup(() => {
+                this._toogleInsert(false);
+            });
         }
         _toogleAdvanced(isOpen) {
             this.setState({
@@ -44,7 +45,7 @@ define([
             return (
                 <div className='ui ui-dialog-menu'>
                     <div className='ui-dialog-menu-item'>
-                        <div className='dialog-label' onClick={() => this._toogleInsert(true)}>
+                        <div className='dialog-label' onClick={() => this._toogleInsert()}>
                             {LANG.insert_object}
                         </div>
                         {this.state.isInsertObjectMenuOpen ? insertObjectPanel : ''}
@@ -53,24 +54,11 @@ define([
             );
         }
 
-        _renderPreview() {
-            return (
-                <PreviewButton />
-            );
-        }
-
         _renderAdvanced() {
-            const advancedPanel = (
-                <AdvancedPanel
-                    onClose={() => this._toogleAdvanced(false)}
-                />
-            );
+            const advancedPanel = <AdvancedPanel onClose={() => this._toogleAdvanced(false)}/>;
             return (
                 <div>
-                    <div
-                        className='option'
-                        onClick={() => this._toogleAdvanced(true)}
-                    >
+                    <div className='option' onClick={() => this._toogleAdvanced()} style={{display: 'inline-block', width: 'unset'}}>
                         {LANG.advanced}
                     </div>
                     {this.state.isAdvancedPanelOpen ? advancedPanel : ''}
@@ -82,7 +70,7 @@ define([
             return (
                 <div className="left-panel">
                     {this._renderInsertObject()}
-                    {this._renderPreview()}
+                    <PreviewButton />
                     {this._renderAdvanced()}
                 </div>
             );
