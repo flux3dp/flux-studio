86aac7f24cbca25b6718ba440f72296946ac5953
Jeff Chen <jeffontheground@gmail.com>
2018-08-16 20:40:21 +0800

    Add film pagination

--- public/js/app/views/beambox/Film-Cutter/Download-Films.jsx
+++ public/js/app/views/beambox/Film-Cutter/Download-Films.jsx
@@ -20,35 +20,8 @@ define([
     i18n
 ) {
     const LANG = i18n.lang.beambox.left_panel.advanced_panel;
+    const Btn = ({children, onClick, className}) => (<div className={'btn btn-default ' + className} onClick={onClick}>{children}</div>);
 
-    const Btn = ({children, onClick}) => (<div className={'btn btn-default'} onClick={onClick}>{children}</div>);
-    const BrandBtns = ({onBrandClick}) => {
-        const brands = FilmDatabase.ls('svg');
-        const btns = brands.map(brand => (
-            <Btn onClick={() => onBrandClick(brand)} key={brand}>{brand}</Btn>
-        ));
-        return <div>{btns}</div>;
-    };
-    const ModelBtns = ({brand, onModelClick}) => {
-        const models = FilmDatabase.ls('svg', brand);
-        const btns = models.map(model => (
-            <Btn onClick={() => onModelClick(model)} key={model}>{model}</Btn>
-        ));
-        return <div>{btns}</div>;
-    };
-    const CategoryBtns = ({brand, model, onCategoryClick}) => {
-        const categories = FilmDatabase.ls('svg', brand, model);
-        const btns = categories
-            .map(cat => cat.replace('.enc', ''))
-            .map(cat => (
-                <Btn onClick={() => onCategoryClick(cat)} key={cat}>{cat}</Btn>
-            ));
-        return <div>{btns}</div>;
-    };
-    const MatchedBtn = ({brand, model, onMatchedClick}) => {
-        const label = `${brand} - ${model}`;
-        return (<Btn onClick={() => onMatchedClick(brand, model)} key={label}>{label}</Btn>);
-    };
     const SearchInput = ({value, onChange}) => {
         return (
             <input
@@ -60,7 +33,7 @@ define([
             />
         );
     };
-
+    const ITEM_PER_PAGE = 28;
     return class DownloadFilms extends React.Component {
         constructor() {
             super();
@@ -69,12 +42,18 @@ define([
                 searchString: '',
                 brand: undefined,
                 model: undefined,
+                source: [],
+                currentPage: 1,
+                pageCount: 1,
             };
         }
         componentWillMount() {
-            this.flushSearchingSource();
+            this.flushFuseSerachingBase();
         }
-        flushSearchingSource() {
+        componentDidMount() {
+            this.updateSource();
+        }
+        flushFuseSerachingBase() {
             const source = FilmDatabase.lsAll('svg');
             this.state.fuse = new Fuse(
                 source,
@@ -92,6 +71,29 @@ define([
                 }
             );
         }
+        sliceToCurrentPage(list) {
+            const currentPage = this.state.currentPage;
+            const start = (currentPage - 1) * ITEM_PER_PAGE;
+            const end = currentPage * ITEM_PER_PAGE;
+            return list.slice(start, end);
+        }
+        updateSource(brand, model) {
+            const newSource = FilmDatabase.ls('svg', brand, model)
+                .map(x => {
+                    if (brand && model) {
+                        return {brand, model, category: x.replace('.enc', '')};
+                    } else if (brand) {
+                        return {brand, model:x};
+                    } else {
+                        return {brand: x};
+                    }
+                });
+            this.setState({
+                source: newSource,
+                currentPage: 1,
+                pageCount: Math.ceil(newSource.length / ITEM_PER_PAGE),
+            });
+        }
         async handleDownloadClick() {
             if(!FilmDatabase.validateUsageDownload()) {
                 AlertActions.showPopupError('film-cutter', '已超过数据下载期限');
@@ -107,7 +109,7 @@ define([
                 await FilmDatabase.syncWithCloud();
                 ProgressActions.close();
                 AlertActions.showPopupInfo('film-cutter', '已成功更新数据');
-                this.flushSearchingSource();
+                this.flushFuseSerachingBase();
             } catch (error) {
                 AlertActions.showPopupError('film-cutter', error.toString());
                 ProgressActions.close();
@@ -118,6 +120,7 @@ define([
             AlertActions.showPopupYesNo('film-cutter', '您确定要重设手机膜吗？这将清空所有已下载的手机模数据。', '重设手机膜', null, {
                 yes: () => {
                     FilmDatabase.reset();
+                    this.flushFuseSerachingBase();
                     this.forceUpdate();
                 },
                 no: () => {}
@@ -146,72 +149,79 @@ define([
         }
         _handleBackClick() {
             if(this.state.model) {
+                this.updateSource(this.state.brand);
                 this.setState({model: undefined});
                 return;
             }
             if(this.state.brand) {
+                this.updateSource();
                 this.setState({brand: undefined});
                 return;
             }
+            this.updateSource();
+        }
+        _handlePageClick(val) {
+            let newPage = this.state.currentPage + val;
+            newPage = Math.min(newPage, this.state.pageCount);
+            newPage = Math.max(newPage, 1);
+            this.setState({currentPage: newPage});
         }
 
-        _renderBrands() {
-            const onBrandClick = (brand) => {
-                this.setState({brand});
-            };
-            return <BrandBtns onBrandClick={onBrandClick}/>;
+        _handleChoiceItemClick(brand, model, category) {
+            if (brand && model && category) {
+                this.handleInsertFile(brand, model, category);
+                return;
+            }
+            this.updateSource(brand, model, category);
+            this.setState({
+                searchString: '',
+                brand,
+                model
+            });
         }
-        _renderModels() {
-            const onModelClick = (model) => {
-                this.setState({model});
-            };
-            return <ModelBtns brand={this.state.brand} onModelClick={onModelClick}/>;
-        }
-        _renderCategories() {
-            const onCategoryClick = (category) => {
-                this.handleInsertFile(this.state.brand, this.state.model, category);
-            };
-            return (
-                <CategoryBtns
-                    brand={this.state.brand}
-                    model={this.state.model}
-                    onCategoryClick={onCategoryClick}
-                />);
-        }
-        _renderMatchedItems() {
-            const items = this.state.fuse.search(this.state.searchString);
-            const onMatchedClick = (brand, model) => {
-                this.setState({
-                    searchString: '',
-                    brand,
-                    model
-                });
-            };
-            const btns = items.map(x =>
-                (<MatchedBtn
-                    brand={x.brand}
-                    model={x.model}
-                    onMatchedClick={onMatchedClick}
-                />));
-            return <div>{btns}</div>;
+
+        handleSearchInputChange(val) {
+            this.setState({
+                searchString: val,
+                brand: undefined,
+                model: undefined,
+            });
+            if (val) {
+                this.setState({source: this.state.fuse.search(val)});
+            } else {
+                this.updateSource();
+            }
         }
         _renderMainContent() {
-            if (this.state.searchString) {
-                return this._renderMatchedItems();
-            }
-
-            if(this.state.brand && this.state.model) {
-                return this._renderCategories();
-            }
-
-            if(this.state.brand) {
-                return this._renderModels();
-            }
-
-            return this._renderBrands();
+            const btns = this.sliceToCurrentPage(this.state.source).map(x => {
+                const label = this.state.searchString ? `${x.brand} - ${x.model}` : (x.category || x.model || x.brand);
+                return (<Btn
+                    className={this.state.searchString ? 'search-result' : ''}
+                    key={label}
+                    onClick={() => this._handleChoiceItemClick(x.brand, x.model, x.category)}
+                >
+                    {label}
+                </Btn>);
+            });
+            return <div>{btns}</div>;
         }
         _renderBanner() {
+            if (this.state.searchString) {
+                return <span/>;
+            }
             return <span>{[this.state.brand, this.state.model].filter(x => !!x).join(' - ') || ' '}</span>;
+        }
+        _renderPagination() {
+            if (this.state.pageCount === 1) {
+                return '';
+            }
+            return (
+                <section className='pagination'>
+                    <Btn onClick={() => this._handlePageClick(-1)}>◀ </Btn>
+                    <span className='pager'>{this.state.currentPage} / {this.state.pageCount}</span>
+                    <Btn onClick={() => this._handlePageClick(1)}> ▶</Btn>
+                </section>
+            );
         }
         render() {
             return (
@@ -222,13 +232,14 @@ define([
                             <div className='title-bar'>
                                 {this._renderBanner()}
                                 <SearchInput
-                                    onChange={e => this.setState({searchString: e.target.value})}
+                                    onChange={e => this.handleSearchInputChange(e.target.value)}
                                     value={this.state.searchString}
                                 />
                             </div>
 
                             {this._renderMainContent()}
                         </section>
+                        {this._renderPagination()}
                         <section className='footer'>
                             <button
                                 className='btn btn-default pull-right'
@@ -250,7 +261,7 @@ define([
                                 onClick={() => {
                                     this._handleBackClick();
                                 }}
-                            >{'上一页'}
+                            >{'上一层'}
                             </button>
                         </section>
                     </div>
