4da56193f365cdb3a93eb2aaa826bf2340645d1d
Net Tseng <b02901184@ntu.edu.tw>
2018-09-04 11:05:21 +0800

    Add default config of Image Trace laser engraving

--- public/js/app/actions/beambox/Laser-Panel-Controller.jsx
+++ public/js/app/actions/beambox/Laser-Panel-Controller.jsx
@@ -15,6 +15,29 @@ define([
         repeat: 1
     }
 
+    const _tracedImageConfig = {
+        speed: 200,
+        strength: 20,
+        repeat: 1
+    }
+
+    const _tracedPathConfig = {
+        speed: 5,
+        strength: 70,
+        repeat: 1
+    }
+
+    const _getConfig = function(name) {
+        switch (name) {
+            case 'Traced Image':
+                return _tracedImageConfig;
+            case 'Traced Path':
+                return _tracedPathConfig;
+            default:
+                return _defaultConfig;
+        }
+    }
+
     const _getLayer = function(name) {
         const layer = $('#svgcontent').find('g.layer').filter(function(){
             return $(this).find('title').text() === name;
@@ -23,7 +46,7 @@ define([
     }
     const _getData = function(name, attr) {
         let val = _getLayer(name).attr('data-' + attr);
-        val = val||_writeData(name, attr, _defaultConfig[attr]);
+        val = val || _writeData(name, attr, _getConfig(name)[attr]);
         return val;
     }
     const _writeData = function(name, attr, val) {
@@ -37,7 +60,7 @@ define([
     const _getStrength = function(name) {
         return _getData(name, 'strength');
     }
-    
+
     const _getRepeat = function(name) {
         return _getData(name, 'repeat');
     }
@@ -52,7 +75,7 @@ define([
 
     const writeRepeat = function(name, val) {
         return _writeData(name, 'repeat', val);
-    }    
+    }
 
     class LaserPanelController {
         constructor() {
@@ -68,9 +91,9 @@ define([
         }
 
         initConfig(name) {
-            _getSpeed(name, _defaultConfig.speed);
-            _getStrength(name, _defaultConfig.strength);
-            _getRepeat(name, _defaultConfig.repeat);
+            _getSpeed(name, _getConfig[name].speed);
+            _getStrength(name, _getConfig[name].strength);
+            _getRepeat(name, _getConfig[name].repeat);
         }
 
         cloneConfig(name, baseName) {
@@ -83,7 +106,7 @@ define([
             const speed = _getSpeed(name);
             const strength = _getStrength(name);
             const repeat = _getRepeat(name);
-            
+
             ReactDOM.render(
                 <LaserPanel
                     layerName={name}
