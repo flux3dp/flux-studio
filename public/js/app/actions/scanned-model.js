6392c7ee66d9c8d9f0fdf3aabf224967703a77b6
Net Tseng <b02901184@ntu.edu.tw>
2018-11-06 16:29:40 +0800

    Fix error message after resizing window

--- public/js/app/actions/scanned-model.js
+++ public/js/app/actions/scanned-model.js
@@ -416,9 +416,10 @@ define([
     }
 
     function onWindowResize() {
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+        if (camera) {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+        }
 
         renderer.setSize(window.innerWidth, window.innerHeight);
 
