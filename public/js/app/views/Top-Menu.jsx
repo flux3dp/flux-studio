9990760a4f1bbfd73c51c53a013e5e8080bae02d
dean sung <a71124725@gmail.com>
2019-07-24 11:06:01 +0800

    Add top menu align icons

--- public/js/app/views/Top-Menu.jsx
+++ public/js/app/views/Top-Menu.jsx
@@ -645,14 +645,14 @@ define([
             _renderTopDropDown: function(id, label) {
                 const labelFunctionMap = {
                     'align-h': [
-                        {id: 'align-h', label: lang.topbar.left_align, f: () => {FnWrapper.alignLeft();}},
+                        {id: 'align-l', label: lang.topbar.left_align, f: () => {FnWrapper.alignLeft();}},
                         {id: 'align-h', label: lang.topbar.center_align, f: () => {FnWrapper.alignCenter();}},
-                        {id: 'align-h', label: lang.topbar.right_align, f: () => {FnWrapper.alignRight();}}
+                        {id: 'align-r', label: lang.topbar.right_align, f: () => {FnWrapper.alignRight();}}
                     ],
                     'align-v': [
-                        {id: 'align-v', label: lang.topbar.top_align, f: () => {FnWrapper.alignTop();}},
+                        {id: 'align-t', label: lang.topbar.top_align, f: () => {FnWrapper.alignTop();}},
                         {id: 'align-v', label: lang.topbar.middle_align, f: () => {FnWrapper.alignMiddle();}},
-                        {id: 'align-v', label: lang.topbar.bottom_align, f: () => {FnWrapper.alignBottom();}}
+                        {id: 'align-b', label: lang.topbar.bottom_align, f: () => {FnWrapper.alignBottom();}}
                     ],
                 };
                 let fns = labelFunctionMap[id];
