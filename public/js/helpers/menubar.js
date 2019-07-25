8d2f0f9659bd8e6d09944dedf32dc343b955a9b9
dean <dean@flux3dp.com>
2019-07-25 17:24:09 +0800

    Update menubar for windows

--- public/js/helpers/menubar.js
+++ public/js/helpers/menubar.js
@@ -1,11 +1,4 @@
 define([
-<<<<<<< HEAD
-], 
-function(
-) {
-    'use strict';
-
-=======
     'jquery',
     'helpers/i18n'
 ], 
@@ -15,56 +8,12 @@ function(
 ) {
     'use strict';
     const LANG = i18n.lang;
->>>>>>> fae51a7a... Update windows frameless browser style
     if (process.platform !== 'win32') return () => {};
 
     return () => {
         const customTitlebar = require('custom-electron-titlebar');
         const Menu = require('electron').remote.Menu;
         const MenuItem = require('electron').remote.MenuItem;
-<<<<<<< HEAD
-
-        const tempMenu = new Menu();
-        tempMenu.append(new MenuItem({
-            label: 'File',
-            submenu: [
-                {
-                    label: 'Subitem 1',
-                    click: () => console.log('Click on subitem 1')
-                },
-                {
-                    type: 'separator'
-                }
-            ]
-        }));
-
-        tempMenu.append(new MenuItem({
-            label: 'Machines',
-            submenu: [
-                {
-                    label: 'Subitem checkbox',
-                    type: 'checkbox',
-                    checked: true
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Subitem with submenu',
-                    submenu: [
-                        {
-                            label: 'Submenu &item 1',
-                            accelerator: 'Ctrl+T'
-                        }
-                    ]
-                }
-            ]
-        }));
-
-        const titlebar = new customTitlebar.Titlebar({
-            backgroundColor: customTitlebar.Color.fromHex('#444'),
-            menu: tempMenu,
-=======
         
         $('.top-menu').css({'-webkit-app-region': 'no-drag'});
         $('.content').css({'height': 'calc(100% - 60px)'});
@@ -72,7 +21,6 @@ function(
         new customTitlebar.Titlebar({
             backgroundColor: customTitlebar.Color.fromHex('#333'),
             shadow: true,
->>>>>>> fae51a7a... Update windows frameless browser style
             menuPosition: 'bottom'
         });
     }
